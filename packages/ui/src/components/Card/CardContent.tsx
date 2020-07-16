a3ec167e8adfd63319c6279535a3c4ff5941cdfe
Чельцов Евгений Олегович <chill.icp@gmail.com>
2020-07-16 18:18:21 +0300

    feat: страница списка заказов

--- packages/ui/src/components/Card/CardContent.tsx
+++ packages/ui/src/components/Card/CardContent.tsx
@@ -9,7 +9,7 @@ export interface CardContentProps {
 const StyledRoot = styled.div<CardContentProps>`
     box-sizing: border-box;
     position: relative;
-    padding: 32px 32px 36px;
+    padding: 32px;
 
     ${({ disabled }) =>
         disabled &&
