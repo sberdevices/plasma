0836ad162f77f0caa31cae19ba713489652b8975
Fanil Zubairov <fannisco@gmail.com>
2020-10-01 12:43:33 +0200

    fix(ui): внешнее API, цвет и фокус компонента Button

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,119 +1,159 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import styled, { css } from 'styled-components';
 import { typography, colors } from 'plasma-tokens';
 
 const sizesToTypography = {
     l: typography.button1,
     m: typography.button1,
-    s: typography.button2
+    s: typography.button2,
 };
 
+// Размеры в пикселях по макету
 const sizes = {
-    l: css`
-        height: 3.5em;
-        padding: 1em 1.625em;
-        border-radius: 1em;`,
-    m: css`
-        height: 3em;
-        padding: 0.75em 1.375em;
-        border-radius: 0.75em;`,
-    s:  css`
-        // Тут у нас жуткие дроби получаются, спрячем их
-        height: ${40/14}em; // 40px@x1
-        padding: ${8/14}em ${18/14}em; // (8px 18px)@x1
-        border-radius: ${12/14}em; // 12px@x1
-`
-};
-
-const sizesIfSquare = {
-    l: css`
-        width: 3.5em;
-        padding-left: 1em;
-        padding-right: 1em;`,
-    m: css`
-        width: 3em;
-        padding-left: 1em;
-        padding-right: 1em;`,
-    s: css`
-        width: ${40/14}em; // 40px@x1
-        padding-left: 1em;
-        padding-right: 1em;`
+    l: {
+        fontSize: 16,
+        height: 56,
+        paddingY: 16,
+        paddingX: 26,
+        squareRadius: 16,
+        outline: 2,
+    },
+    m: {
+        fontSize: 16,
+        height: 48,
+        paddingY: 12,
+        paddingX: 22,
+        squareRadius: 12,
+        outline: 2,
+    },
+    s: {
+        fontSize: 14,
+        height: 40,
+        paddingY: 8,
+        paddingX: 18,
+        squareRadius: 12,
+        outline: 2,
+    },
 };
 
 const viewToColors = {
     primary: css`
-        background-color: ${colors.accent};
+        background-color: ${colors.buttonAccent};
         color: ${colors.text};
-
-        &:focus {
-            box-shadow: 0 0 0 3px ${colors.background}, 0 0 0 6px ${colors.accent};
-        }`,
+    `,
     secondary: css`
         background-color: ${colors.buttonSecondary};
         color: ${colors.text};
-
-        &:focus {
-            box-shadow: 0 0 0 3px ${colors.background}, 0 0 0 6px ${colors.accent};
-        }`,
+    `,
     checked: css`
         background-color: ${colors.buttonChecked};
         color: ${colors.black};
-
-        &:focus {
-            box-shadow: 0 0 0 3px ${colors.background}, 0 0 0 6px ${colors.buttonChecked};
-        }`,
+    `,
     warning: css`
         background-color: ${colors.buttonWarning};
         color: ${colors.text};
-
-        &:focus {
-            box-shadow: 0 0 0 3px ${colors.background}, 0 0 0 6px ${colors.buttonWarning};
-        }`,
+    `,
     critical: css`
         background-color: ${colors.buttonCritical};
         color: ${colors.text};
-
-        &:focus {
-            box-shadow: 0 0 0 3px ${colors.background}, 0 0 0 6px ${colors.buttonCritical};
-        }`,
+    `,
     clear: css`
-        background-color: transparent;
+        background-color: ${colors.transparent};
         color: ${colors.text};
-
-        &:focus {
-            box-shadow: 0 0 0 3px ${colors.background}, 0 0 0 6px ${colors.buttonSecondary};
-        }`
+    `,
 };
 
-const pins = {
-    'clear-clear': css`
-        border-radius: 0;`,
-    'square-square': css``,
-    'clear-square': css`
-        border-top-left-radius: 0;
-        border-bottom-left-radius: 0;`,
-    'square-clear': css`
-        border-top-right-radius: 0;
-        border-bottom-right-radius: 0;`,
-    'circle-circle': css`
-        border-radius: 1.75em;`,
-    'clear-circle': css`
-        border-radius: 0 1.75em 1.75em 0;`,
-    'circle-clear': css`
-        border-radius: 1.75em 0 0 1.75em;`
+// Матрица радиусов. r - радиус стандартный, h - радиус округлый (вычисляемый из высоты)
+const pinsMatrix = {
+    'square-square': 'r r r r',
+    'square-clear': 'r 0 0 r',
+    'clear-square': '0 r r 0',
+    'clear-clear': '0 0 0 0',
+    'clear-circle': '0 h h 0',
+    'circle-clear': 'h 0 0 h',
+    'circle-circle': 'h h h h',
 };
 
 export type Sizes = keyof typeof sizesToTypography;
 
 export type Views = keyof typeof viewToColors;
 
-export type Pins = keyof typeof pins;
+export type Pins = keyof typeof pinsMatrix;
 
-interface StyledButtonProps {
-    isText?: boolean;
-    pin?: Pins;
-    view?: Views;
+export interface SizesModel {
     size?: Sizes;
+    pin?: Pins;
+    isText?: boolean;
+}
+
+// Преобразовывает матрицу радиусов с указанными размерами
+const convertMatrix = (matrix: string, r: string, h: string): string => {
+    return matrix
+        .split(' ')
+        .map((char) => {
+            switch (char) {
+                case 'r':
+                    return r;
+                case 'h':
+                    return h;
+                default:
+                    return char;
+            }
+        })
+        .join(' ');
+};
+
+// Возвращает стили размеров по параметрам
+const getSizes = ({ size = 'l', pin = 'square-square', isText = false }: SizesModel) => {
+    const fontSize = sizes[size].fontSize;
+    const height = sizes[size].height / fontSize;
+    const paddingY = sizes[size].paddingY / fontSize;
+    const paddingX = sizes[size].paddingX / fontSize;
+    const squareRadius = sizes[size].squareRadius / fontSize;
+    const circleRadius = height / 2;
+    const outline = sizes[size].outline / fontSize;
+    const elemRadius = convertMatrix(pinsMatrix[pin], `${squareRadius}em`, `${circleRadius}em`);
+    const beforeRadius = convertMatrix(pinsMatrix[pin], `${squareRadius + outline}em`, `${circleRadius + outline}em`);
+
+    let sizesIfText;
+
+    if (isText) {
+        sizesIfText = css`
+            padding-left: ${paddingX}em;
+            padding-right: ${paddingX}em;
+        `;
+    } else {
+        sizesIfText = css`
+            width: ${height}em;
+            padding-left: ${paddingY}em;
+            padding-right: ${paddingY}em;
+        `;
+    }
+
+    return css`
+        ${sizesIfText};
+
+        height: ${height}em;
+        padding-top: ${paddingY}em;
+        padding-bottom: ${paddingY}em;
+        border-radius: ${elemRadius};
+
+        ${sizesToTypography[size]}
+
+        &::before {
+            top: -${outline}em;
+            left: -${outline}em;
+            border-radius: ${beforeRadius};
+            border-width: ${outline}em;
+        }
+        &:focus::before {
+            box-shadow: 0 0 0 ${outline}em ${colors.buttonFocused};
+        }
+    `;
+};
+
+interface StyledButtonProps extends SizesModel {
+    view?: Views;
     disabled?: boolean;
 }
 
@@ -121,7 +161,6 @@ const StyledButton = styled.button<StyledButtonProps>`
     position: relative;
 
     display: inline-flex;
-    justify-content: center;
     align-items: center;
 
     appearance: none;
@@ -145,30 +184,28 @@ const StyledButton = styled.button<StyledButtonProps>`
         ${viewToColors[view]}
     `}
 
-    ${({ size = 'l', isText }) => css`
-        ${sizes[size]}
-        ${sizesToTypography[size]}
-        ${!isText && sizesIfSquare[size]}
-    `}
-
-    ${({ pin }) => pin && css`
-        ${pins[pin]};
-    `}
-
-    ${({ disabled }) => disabled && css`
-        opacity: 0.4;
-
-        &:hover, &:active {
-            transform: none;
-        }
-    `}
+    ${getSizes}
 
     &[disabled] {
         opacity: 0.4;
 
-        &:hover, &:active {
+        &:hover,
+        &:active {
             transform: none;
         }
+    }
+
+    &::before {
+        position: absolute;
+
+        width: 100%;
+        height: 100%;
+
+        border-color: transparent;
+        border-style: solid;
+        content: '';
+
+        transition: box-shadow 0.2s ease-in-out;
     }
 `;
 
@@ -178,60 +215,53 @@ interface StyledTextProps {
 }
 
 const StyledText = styled.span<StyledTextProps>`
-    ${({ isIconLeft }) => isIconLeft && css`
-        margin-left: 0.25em;
-    `}
+    ${({ isIconLeft }) =>
+        isIconLeft &&
+        css`
+            margin-left: 0.25em;
+        `}
 
-    ${({ isIconRight }) => isIconRight && css`
-        margin-right: 0.25em;
-    `}
+    ${({ isIconRight }) =>
+        isIconRight &&
+        css`
+            margin-right: 0.25em;
+        `}
 `;
 
-export interface ButtonProps extends Pick<StyledButtonProps, 'pin' | 'view' | 'size' | 'disabled'>, React.HTMLAttributes<HTMLElement> {
+export interface ButtonProps
+    extends Pick<StyledButtonProps, 'pin' | 'view' | 'size' | 'disabled'>,
+        React.ButtonHTMLAttributes<HTMLButtonElement> {
     iconLeft?: React.ReactElement;
     iconRight?: React.ReactElement;
     children?: string;
     className?: string;
-    onFocus?: React.FocusEventHandler<HTMLElement>;
-    onBlur?: React.FocusEventHandler<HTMLElement>;
-    onClick?: React.MouseEventHandler<HTMLElement>;
+    onFocus?: React.FocusEventHandler<HTMLButtonElement>;
+    onBlur?: React.FocusEventHandler<HTMLButtonElement>;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-    view,
-    size,
-    pin,
-    disabled,
-    children,
-    iconLeft,
-    iconRight,
-    className,
-    style,
-    onFocus,
-    onBlur,
-    onClick
-}) => (
-    <StyledButton
-        className={className}
-        style={style}
-        view={view}
-        size={size}
-        pin={pin}
-        disabled={disabled}
-        isText={!!children}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onClick={onClick}
-    >
-        {iconLeft}
-        {children && (
-            <StyledText
-                isIconLeft={!!iconLeft}
-                isIconRight={!!iconRight}
-            >
-                {children}
-            </StyledText>
-        )}
-        {iconRight}
-    </StyledButton>
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ view, size, pin, disabled, children, iconLeft, iconRight, className, style, onFocus, onBlur, onClick }, ref) => (
+        <StyledButton
+            className={className}
+            style={style}
+            view={view}
+            size={size}
+            pin={pin}
+            disabled={disabled}
+            isText={!!children}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            onClick={onClick}
+            ref={ref}
+        >
+            {iconLeft}
+            {children && (
+                <StyledText isIconLeft={!!iconLeft} isIconRight={!!iconRight}>
+                    {children}
+                </StyledText>
+            )}
+            {iconRight}
+        </StyledButton>
+    ),
 );
