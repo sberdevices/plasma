9892f06dae530f005ebe461d2fc29890db2b731d
Igor Antonov <aii.cs.web@gmail.com>
2020-10-02 08:34:31 +0300

    feat(ui): add theme switcher in toolbar

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -8,5 +8,6 @@ module.exports = {
         '@storybook/addon-viewport/register',
         '@storybook/addon-knobs/register',
         '@storybook/addon-actions',
+        '@storybook/addon-toolbars',
     ],
 };
--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,7 +1,55 @@
-import { addParameters } from '@storybook/react';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { addDecorator, addParameters } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { ThemeDarkEva, ThemeDarkJoy, ThemeDarkSber } from 'plasma-styles/components/Theme';
+import Typo from 'plasma-styles/components/Typo';
+import Color from 'plasma-styles/components/Color';
+import {
+    colorTextPrimary,
+    colorBackgroundColor,
+    colorBackgroundDefault,
+} from 'plasma-styles/components/Color/tokens';
 
+const DocumentStyle = createGlobalStyle`
+    /* stylelint-disable-next-line selector-nested-pattern */
+    html {
+        height: 100vh;
+    }
+
+    /* stylelint-disable-next-line selector-nested-pattern */
+    body {
+        height: 100vh;
+        color: ${colorTextPrimary};
+        background: ${colorBackgroundDefault};
+        background-color: ${colorBackgroundColor};
+        margin: 0;
+    }
+`;
+
+const withTheme = (Story, context) => {
+    const mapThemeOnComponent = {
+        sber: ThemeDarkSber,
+        eva: ThemeDarkEva,
+        joy: ThemeDarkJoy,
+    };
+    const Theme = mapThemeOnComponent[context.globals.theme];
+    return (
+        <>
+            <Theme />
+            <Typo />
+            <Color />
+            <DocumentStyle />
+            <Story {...context} />
+        </>
+    );
+};
+
+addDecorator(withKnobs);
+addDecorator(withTheme);
 
 addParameters({
+    options: { theme: {} },
     viewport: {
         defaultViewport: 'HD',
         viewports: {
@@ -28,13 +76,15 @@ addParameters({
             },
         },
     },
-    backgrounds: {
-        default: 'dark',
-        values: [
-            {
-                name: 'dark',
-                value: '#0B121E',
-            },
-        ],
-    },
 });
+
+export const globalTypes = {
+    theme: {
+        name: 'Theme',
+        description: 'Global theme for components',
+        defaultValue: 'sber',
+        toolbar: {
+            items: ['sber', 'joy', 'eva'],
+        },
+    },
+};
