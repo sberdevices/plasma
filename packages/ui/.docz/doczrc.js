6b971935fdf58ead469f109e227c30e756dcd3b0
Igor Antonov <aii.cs.web@gmail.com>
2020-10-05 13:31:40 +0300

    chore(ui): add docz to gitignore

--- packages/ui/.docz/doczrc.js
+++
@@ -1,5 +0,0 @@
-export default {
-    typescript: true,
-    filterComponents: (files) =>
-        files.filter((filepath) => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath) && !filepath.includes('stories')),
-};
