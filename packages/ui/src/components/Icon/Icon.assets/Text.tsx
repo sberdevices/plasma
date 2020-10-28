449091640fd5314c52f18dcc05a0fc6cd315a740
IgorAntonov <aii.cs.web@gmail.com>
2020-10-28 09:31:59 +0300

    refactor(ui): fix another lint errors

--- packages/ui/src/components/Icon/Icon.assets/Text.tsx
+++ packages/ui/src/components/Icon/Icon.assets/Text.tsx
@@ -4,9 +4,8 @@ import { IconAsset } from '../IconRoot';
 
 export const Text: React.FC<IconAsset> = (props) => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path d="M4 6H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M4 12H14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M4 18H18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M4 6H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M4 12H14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M4 18H18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
-
