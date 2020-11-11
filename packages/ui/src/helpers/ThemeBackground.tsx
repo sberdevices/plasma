5a257650aefae2f1c1b56e0123fe7fa4cbc1fe75
Fanil Zubairov <fannisco@gmail.com>
2020-11-11 11:47:57 +0200

    feat(ui): Доработан хелпер ThemeBackground

--- packages/ui/src/helpers/ThemeBackground.tsx
+++ packages/ui/src/helpers/ThemeBackground.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { text, background, gradient } from '@sberdevices/plasma-tokens';
 
-const StyledRoot = styled.div`
+interface Props {
+    spaced?: boolean;
+}
+
+const StyledRoot = styled.div<Props>`
     display: flex;
     box-sizing: border-box;
 
@@ -12,8 +16,16 @@ const StyledRoot = styled.div`
     color: ${text};
     background-color: ${background};
     background-image: ${gradient};
+
+    ${({ spaced }) =>
+        spaced &&
+        css`
+            & > * {
+                margin-right: 0.5rem;
+            }
+        `}
 `;
 
-export const ThemeBackground: React.FC = ({ children }) => {
-    return <StyledRoot>{children}</StyledRoot>;
+export const ThemeBackground: React.FC<Props> = ({ spaced, children }) => {
+    return <StyledRoot spaced={spaced}>{children}</StyledRoot>;
 };
