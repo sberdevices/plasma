14d5c6de18768e5433bd7eb871949eef01503e9b
IgorAntonov <aii.cs.web@gmail.com>
2020-11-16 11:53:48 +0300

    fix: Убран необязательный экспорт

--- packages/ui/src/components/Button/index.ts
+++ packages/ui/src/components/Button/index.ts
@@ -1,7 +1,7 @@
 export { Button, sizes as buttonSizes } from './Button';
 export type { ButtonProps } from './Button';
 
-export { ActionButton, colorsToViews as actionButtonColorsToViews } from './ActionButton';
+export { ActionButton } from './ActionButton';
 export type { ActionButtonProps } from './ActionButton';
 
 export { buttonBase, buttonTypography, fontSizeL, fontSizeM, fontSizeS } from './ButtonBase';
