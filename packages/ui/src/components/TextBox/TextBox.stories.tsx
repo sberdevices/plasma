e74b64d4fcd51b4f0fe0bbae19c43c6be41f287d
Fanil Zubairov <fannisco@gmail.com>
2020-11-19 10:51:25 +0200

    fix(ui): Исправлено неправильное использование сетки в сторибуке

--- packages/ui/src/components/TextBox/TextBox.stories.tsx
+++ packages/ui/src/components/TextBox/TextBox.stories.tsx
@@ -4,7 +4,7 @@ import { accent, footnote1 } from '@sberdevices/plasma-tokens';
 import { text } from '@storybook/addon-knobs';
 
 import { Footnote1 } from '../Typography';
-import { Container, Col } from '../Grid';
+import { Container, Row, Col } from '../Grid';
 
 import { TextBox, Title, BigTitle, SubTitle, Label, Caption } from './TextBox';
 
@@ -57,61 +57,75 @@ export const Variations = () => {
 
     return (
         <Container>
-            <Col size={3}>
-                <Example>
-                    <Variation>Title+Subtitle</Variation>
-                    <TextBox>
-                        <Title>{titleText}</Title>
-                        <SubTitle>{subTitleText}</SubTitle>
-                    </TextBox>
-                </Example>
-            </Col>
-            <Col size={3}>
-                <Example>
-                    <Variation>Title+Subtitle+Caption</Variation>
-                    <TextBox>
-                        <Title>{titleText}</Title>
-                        <SubTitle>{subTitleText}</SubTitle>
-                        <Caption>{captionText}</Caption>
-                    </TextBox>
-                </Example>
-            </Col>
-            <Col size={3}>
-                <Example>
-                    <Variation>Label+Title</Variation>
-                    <TextBox label={labelText} title={titleText} />
-                </Example>
-            </Col>
-            <Col size={3}>
-                <Example>
-                    <Variation>Label+BigTitle</Variation>
-                    <TextBox>
-                        <Label>{labelText}</Label>
-                        <BigTitle>{bigTitleText}</BigTitle>
-                    </TextBox>
-                </Example>
-            </Col>
-            <Col size={3}>
-                <Example>
-                    <Variation>Caption+Title</Variation>
-                    <TextBox>
-                        <Caption>{captionText}</Caption>
-                        <Title>{titleText}</Title>
-                    </TextBox>
-                </Example>
-            </Col>
-            <Col size={3}>
-                <Example>
-                    <Variation>BigTitle+Subtitle</Variation>
-                    <TextBox size="l" title={bigTitleText} subTitle={subTitleText} />
-                </Example>
-            </Col>
-            <Col size={3}>
-                <Example>
-                    <Variation>Laber+Title+Subtitle</Variation>
-                    <TextBox label={labelText} title={titleText} subTitle={subTitleText} />
-                </Example>
-            </Col>
+            <Row>
+                <Col size={3}>
+                    <Example>
+                        <Variation>Title+Subtitle</Variation>
+                        <TextBox>
+                            <Title>{titleText}</Title>
+                            <SubTitle>{subTitleText}</SubTitle>
+                        </TextBox>
+                    </Example>
+                </Col>
+            </Row>
+            <Row>
+                <Col size={3}>
+                    <Example>
+                        <Variation>Title+Subtitle+Caption</Variation>
+                        <TextBox>
+                            <Title>{titleText}</Title>
+                            <SubTitle>{subTitleText}</SubTitle>
+                            <Caption>{captionText}</Caption>
+                        </TextBox>
+                    </Example>
+                </Col>
+            </Row>
+            <Row>
+                <Col size={3}>
+                    <Example>
+                        <Variation>Label+Title</Variation>
+                        <TextBox label={labelText} title={titleText} />
+                    </Example>
+                </Col>
+            </Row>
+            <Row>
+                <Col size={3}>
+                    <Example>
+                        <Variation>Label+BigTitle</Variation>
+                        <TextBox>
+                            <Label>{labelText}</Label>
+                            <BigTitle>{bigTitleText}</BigTitle>
+                        </TextBox>
+                    </Example>
+                </Col>
+            </Row>
+            <Row>
+                <Col size={3}>
+                    <Example>
+                        <Variation>Caption+Title</Variation>
+                        <TextBox>
+                            <Caption>{captionText}</Caption>
+                            <Title>{titleText}</Title>
+                        </TextBox>
+                    </Example>
+                </Col>
+            </Row>
+            <Row>
+                <Col size={3}>
+                    <Example>
+                        <Variation>BigTitle+Subtitle</Variation>
+                        <TextBox size="l" title={bigTitleText} subTitle={subTitleText} />
+                    </Example>
+                </Col>
+            </Row>
+            <Row>
+                <Col size={3}>
+                    <Example>
+                        <Variation>Laber+Title+Subtitle</Variation>
+                        <TextBox label={labelText} title={titleText} subTitle={subTitleText} />
+                    </Example>
+                </Col>
+            </Row>
         </Container>
     );
 };
