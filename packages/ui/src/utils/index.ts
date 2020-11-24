2d574346bd11468dfb764aa3e78853efd2e8708d
Fanil Zubairov <fannisco@gmail.com>
2020-11-24 11:37:04 +0200

    refactor(ui): utils/index

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,5 +1,10 @@
+export { animatedScrollToX } from './animatedScrollTo';
+
 export { convertRoundnessMatrix } from './roundness';
 export type { PinProps } from './roundness';
+
 export { detectDevice, deviceScales, isSberBox, isSberPortal } from './deviceDetection';
 export type { DeviceKind } from './deviceDetection';
+
 export { mediaQuery, breakpoints } from './mediaQuery';
+export type { Breakpoint } from './mediaQuery';
