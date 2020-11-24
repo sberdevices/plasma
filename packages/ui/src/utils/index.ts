374b7d335b2b173edf4c24476042097865c794c8
Fanil Zubairov <fannisco@gmail.com>
2020-11-24 10:01:18 +0200

    refactor(ui): mixins/pins => utils/roundness

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,3 +1,5 @@
+export { convertRoundnessMatrix } from './roundness';
+export type { PinProps } from './roundness';
 export { detectDevice, deviceScales, isSberBox, isSberPortal } from './deviceDetection';
 export type { DeviceKind } from './deviceDetection';
 export { mediaQuery, breakpoints } from './mediaQuery';
