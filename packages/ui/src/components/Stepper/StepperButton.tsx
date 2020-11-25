5cd0d3bcf0d39fb2c5c21029145f0b6fcc69e947
Fanil Zubairov <fannisco@gmail.com>
2020-11-25 11:02:26 +0200

    refactor(ui): Разложил Stepper по компонентам, тайпинги

---
+++ packages/ui/src/components/Stepper/StepperButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { PickOptional } from '../../types/PickOptional';
+import { ActionButton, ButtonProps } from '../Button';
+
+export interface StepperButtonProps
+    extends PickOptional<
+        ButtonProps,
+        'pin' | 'view' | 'disabled' | 'className' | 'style' | 'onFocus' | 'onBlur' | 'onClick'
+    > {
+    icon?: React.ReactElement;
+}
+
+export const StepperButton: React.FC<StepperButtonProps> = ({
+    pin = 'circle-circle',
+    view = 'secondary',
+    icon,
+    ...rest
+}) => (
+    <ActionButton size="m" pin={pin} view={view} {...rest}>
+        {icon}
+    </ActionButton>
+);
---
+++ packages/ui/src/components/Stepper/StepperRoot.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StepperRoot = styled.div`
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    justify-content: space-between;
+    width: max-content;
+`;
