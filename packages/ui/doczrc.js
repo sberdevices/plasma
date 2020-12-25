09b121f84c11067dbfeb8dde98286a520d528aab
IgorAntonov <aii.cs.web@gmail.com>
2020-12-25 15:00:43 +0300

    ci: добавлен билд документации

--- packages/ui/doczrc.js
+++ packages/ui/doczrc.js
@@ -1,6 +1,8 @@
 export default {
     typescript: true,
     title: 'Plasma UI Kit',
+    base: '/pr1/',
+    dest: './.docz/dist/pr1/',
     filterComponents: (files) =>
         files.filter((filepath) => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath) && !filepath.includes('stories')),
 };
