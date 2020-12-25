92434f854acee086d267edb682eb43c6d6851160
IgorAntonov <aii.cs.web@gmail.com>
2020-12-25 15:03:23 +0300

    ci: убран хардкод номера PR

--- packages/ui/doczrc.js
+++ packages/ui/doczrc.js
@@ -1,8 +1,6 @@
 export default {
     typescript: true,
     title: 'Plasma UI Kit',
-    base: '/pr1/',
-    dest: './.docz/dist/pr1/',
     filterComponents: (files) =>
         files.filter((filepath) => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath) && !filepath.includes('stories')),
 };
