30c7f251c65008ba8734406a2e84a24c57f266d4
IgorAntonov <aii.cs.web@gmail.com>
2021-01-19 14:56:59 +0300

    docs(ui): Добавлена документация для DeviceDetection

--- packages/ui/src/components/Device/DeviceDetection.tsx
+++ packages/ui/src/components/Device/DeviceDetection.tsx
@@ -25,10 +25,40 @@ const typoSizes = {
 /* stylelint-enable */
 
 export interface DeviceThemeProps {
+    /**
+     * Объект темы для стилизованных компонентов.
+     *
+     * Внутри `DeviceThemeProvider` использует `styled-components/ThemeProvider`
+     * и расширяет объект темы полем `deviceScale`.
+     */
     theme?: object;
+    /**
+     * Пользовательская функция определения устройства.
+     *
+     * Должна возвращать 3 возможных значения - `sberBox` или `sberPortal` или `touch`.
+     *
+     * Возвращаемое значение  по умолчанию - `sberBox`.
+     *
+     * При значениях `sberBox` и `sberPortal` типографика примет размер **x2**, а при `touch` - **x1**.
+     */
     detectDeviceCallback?: () => DeviceKind;
 }
 
+/**
+ * Провайдер обязателен для использования, необходимо обернуть в него все приложение.
+ *
+ * Назначение - определяет, на какой *поверхности* (**устройстве**) запущено приложение.
+ *
+ * В зависимости от поверхности, применяет глобальные стили типографики,
+ * подходящие данной платформе, ко всему приложению.
+ *
+ * Функцией определения по умолчанию тип поверхности определяется на основании `userAgent`,
+ * при отсутствии `navigator` или `userAgent`, поверхность будет определен как `sberBox`.
+ *
+ * Имеется возможность передать провайдеру собственную логику определения девайса
+ * с помощью пропса `detectDeviceCallback`.
+ * При этом стоит помнить, что разрешены только 3 стандартных значения.
+ */
 export const DeviceThemeProvider: React.FC<DeviceThemeProps> = ({
     theme,
     children,
@@ -37,6 +67,7 @@ export const DeviceThemeProvider: React.FC<DeviceThemeProps> = ({
     const deviceKind = detectDeviceCallback();
     const deviceScale = deviceScales[deviceKind] || sberPortalScale;
     const Typo = React.useMemo(() => typoSizes[deviceKind], [deviceKind]);
+
     return (
         <ThemeProvider theme={{ ...theme, deviceScale }}>
             <Typo />
