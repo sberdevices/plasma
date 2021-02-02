a1deaaa3fb9ed4ca863d3f31c414f45407d8dc0b
Fanil Zubairov <fannisco@gmail.com>
2021-02-02 15:54:33 +0200

    feat(ui): Компонент Switch, #151

---
+++ packages/ui/src/components/Switch/Switch.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { boolean, text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import { Switch } from '.';
+
+const onChangeAction = action('onChange');
+const onFocusAction = action('onFocus');
+const onBlurAction = action('onBlur');
+
+export default {
+    title: 'Switch',
+    component: Switch,
+};
+
+export const Default = () => {
+    const [checked, setChecked] = React.useState(true);
+
+    return (
+        <div style={{ maxWidth: '20rem' }}>
+            <Switch
+                id="agreement"
+                name="agreement"
+                value="yes"
+                label={text('label', 'Label with long text that will be shortened to fit the with')}
+                outlined={boolean('outlined', false)}
+                focused={boolean('focused', false)}
+                checked={checked}
+                disabled={boolean('disabled', false)}
+                onChange={(event) => {
+                    setChecked(event.target.checked);
+                    onChangeAction(event.target.value, event.target.checked);
+                }}
+                onFocus={() => onFocusAction()}
+                onBlur={() => onBlurAction()}
+            />
+        </div>
+    );
+};
