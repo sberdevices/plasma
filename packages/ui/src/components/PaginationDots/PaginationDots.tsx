a1999edeafb3b2ac64e10c103be82bde79a91d53
Fanil Zubairov <fannisco@gmail.com>
2021-02-03 12:25:53 +0200

    feat(ui): Компонент PaginationDots, #152

---
+++ packages/ui/src/components/PaginationDots/PaginationDots.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+/**
+ * Оборачивающий компонент для пагинации точками.
+ * Стилизованный компонент, обладающий всеми свойствами ``div``.
+ */
+export const PaginationDots = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    align-self: center;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+`;
