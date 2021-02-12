1e7eba1ca7226e0e8d3a4890c2ab488063b32a4c
Fanil Zubairov <fannisco@gmail.com>
2021-02-12 11:24:25 +0200

    feat(ui): Toast добавлена анимация появления / исчезания

--- packages/ui/src/components/Toast/ToastController.tsx
+++ packages/ui/src/components/Toast/ToastController.tsx
@@ -1,55 +1,89 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import styled, { css, keyframes } from 'styled-components';
 
 import { ToastInfo } from './types';
 import { Toast } from './Toast';
 import { useToast } from './useToast';
 
-const showAnimation = keyframes`
+const showAnimation = (position: string) => keyframes`
     0% {
+        transform: translate(-50%, ${position === 'top' && '-'}5rem);
         opacity: 0;
+    }
+
+    80% {
+        transform: translate(-50%, 0);
+        opacity: 0.7;
     }
 
     100% {
         opacity: 1;
     }
 `;
+const hideAnimation = (position: string) => keyframes`
+    0% {
+        opacity: 1;
+    }
 
-const StyledRoot = styled.div<{ vertical: string }>`
-    position: fixed;
-    left: 50%;
-    transform: translateX(-50%);
+    20% {
+        transform: translate(-50%, 0);
+        opacity: 0.7;
+    }
 
-    ${({ vertical }) => css`
-        ${vertical}: 1.5rem;
-    `};
-
-    animation: 0.3s ${showAnimation} ease-out;
-    z-index: 1000;
+    100% {
+        transform: translate(-50%, ${position === 'top' && '-'}5rem);
+        opacity: 0;
+    }
 `;
 
+const StyledRoot = styled.div<{ position: string; isVisible: boolean }>`
+    position: fixed;
+    left: 50%;
+    z-index: 1000;
+    transform: translateX(-50%);
+
+    ${({ position, isVisible }) => css`
+        ${position}: 5rem;
+        animation: 300ms ${isVisible ? showAnimation(position) : hideAnimation(position)};
+    `};
+`;
+
+/**
+ * Создаёт <div />, который внутри себя содержит тост.
+ * Цикл: show => timeout => hide.
+ */
 export const ToastController: React.FC<ToastInfo> = ({ text, position = 'bottom', timeout }) => {
+    const { hideToast } = useToast();
+    const [isVisible, setIsVisible] = useState(true);
+    const hideTimeout = useRef<number | null>(null);
     const toastKey = `${text}${position}`;
 
-    const { hideToast } = useToast();
+    const animationEndHandler = useCallback(() => {
+        if (!isVisible) {
+            hideToast();
+            setIsVisible(true); // Необходимо вернуть булево к следующему вызову тоста
+        }
+        if (isVisible) {
+            hideTimeout.current = setTimeout(() => {
+                setIsVisible(false);
+            }, timeout);
+        }
+    }, [timeout, isVisible]);
 
     useEffect(() => {
-        if (!text || timeout === null) {
-            return undefined;
-        }
-        const hideTimeout = setTimeout(() => {
-            hideToast();
-        }, timeout);
-
-        return () => clearTimeout(hideTimeout);
-    }, [toastKey, text, timeout]);
+        return () => {
+            if (hideTimeout.current !== null) {
+                clearTimeout(hideTimeout.current);
+            }
+        };
+    }, []);
 
     if (!text || !position) {
         return null;
     }
 
     return (
-        <StyledRoot key={toastKey} vertical={position}>
+        <StyledRoot key={toastKey} position={position} isVisible={isVisible} onAnimationEnd={animationEndHandler}>
             <Toast text={text} />
         </StyledRoot>
     );
