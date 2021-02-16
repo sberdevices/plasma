5a07a00855fe485084b03cdfc87c9ca8d2ad66f0
Fanil Zubairov <fannisco@gmail.com>
2021-02-16 14:06:02 +0200

    fix(ui): Carousel исправление overscroll-отступов

--- packages/ui/src/components/Carousel/CarouselGridWrapper.tsx
+++ packages/ui/src/components/Carousel/CarouselGridWrapper.tsx
@@ -1,24 +1,12 @@
 import styled from 'styled-components';
 
-import { StyledCarousel, StyledCarouselTrack } from './Carousel';
-
 /**
  * Компонент применяется, если требуется компенсировать отступы контейнера в сетке.
+ * При обертывании вокруг ``Carousel``, добавляет карусели и ее прокрутке дополнительные отступы.
  * Стилизованный компонент, обладающий всеми свойствами ``div``.
  */
 export const CarouselGridWrapper = styled.div`
     overflow: hidden;
     margin-left: calc(var(--plasma-grid-margin) * -1);
     margin-right: calc(var(--plasma-grid-margin) * -1);
-
-    /* stylelint-disable-next-line selector-nested-pattern */
-    ${StyledCarousel} {
-        scroll-padding: 0 var(--plasma-grid-margin);
-        padding-left: var(--plasma-grid-margin);
-    }
-
-    /* stylelint-disable-next-line selector-nested-pattern */
-    ${StyledCarouselTrack} {
-        padding-right: var(--plasma-grid-margin);
-    }
 `;
