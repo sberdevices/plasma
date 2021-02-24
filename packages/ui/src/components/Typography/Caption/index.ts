9b92ffc38c9a30c02f442e4bc88abcb3acfaad59
out-sustavov-av <avsustavov@sberbank.ru>
2021-02-24 13:58:42 +0300

    feat(ui): Добавлено правило переноса строк для типографики

--- packages/ui/src/components/Typography/Caption/index.ts
+++ packages/ui/src/components/Typography/Caption/index.ts
@@ -1,4 +1,4 @@
 import styled from 'styled-components';
 import { caption } from '@sberdevices/plasma-tokens';
 
-export const Caption = styled.div(caption);
+export const Caption = styled.div({ overflowWrap: 'break-word', hyphens: 'auto', ...caption });
