109cce99259014f3be24f915c02ff287c24d3dc2
Fanil Zubairov <fannisco@gmail.com>
2021-02-24 14:04:51 +0200

    feat: В компонент Core / Button занесен основной функционал

---
+++ packages/plasma-core/src/components/Button/Button.types.ts
@@ -0,0 +1,99 @@
+import type { ReactNode } from 'react';
+
+import type { DisabledProps, FocusProps, OutlinedProps } from '../../mixins';
+import type { PinProps } from '../../utils';
+import type { ShiftProps, AsProps } from '../../types';
+
+/**
+ * Размеры кнопки
+ */
+interface SizeProps<T> {
+    /**
+     * Размер кнопки
+     */
+    size: T;
+    /**
+     * Квадратная кнопка (со сторанами 1:1)
+     */
+    square?: boolean;
+    /**
+     * Растягиваемость кнопки
+     */
+    resizible?: boolean;
+}
+
+/**
+ * Виды кнопки
+ */
+export interface ViewProps<T> {
+    /**
+     * Вид кнопки
+     */
+    view: T;
+}
+
+/**
+ * С текстом и/или контентом слева.
+ */
+export interface WithTextAndContentLeft {
+    /**
+     * Текстовая надпись на кнопке
+     */
+    text: string | number;
+    /**
+     * Кастомный контент кнопки. При указании этого свойства contentLeft, contentRight и text не применяются
+     */
+    children?: never;
+    /**
+     * Слот для контента слева, например Icon
+     */
+    contentLeft?: ReactNode;
+}
+
+/**
+ * С текстом и/или контентом справа.
+ */
+export interface WithTextAndContentRight {
+    text: string | number;
+    children?: never;
+    /**
+     * Слот для контента справа, например Icon
+     */
+    contentRight?: ReactNode;
+}
+
+/**
+ * С контентом слева.
+ */
+export interface WithContentLeft {
+    children?: never;
+    contentLeft: ReactNode;
+}
+
+/**
+ * Через ``children``.
+ */
+export interface WithChildren {
+    children: ReactNode;
+}
+
+/**
+ * Интерфейс для обхода контентных опций
+ */
+export interface AllContentProps
+    extends WithTextAndContentLeft,
+        Pick<WithTextAndContentRight, 'contentRight'>,
+        Pick<WithTextAndContentRight, 'children'> {}
+
+/**
+ * Интерфейс для стилизованного компонента, не включая контентных
+ */
+export interface StyledButtonProps<S, V>
+    extends SizeProps<S>,
+        ViewProps<V>,
+        PinProps,
+        FocusProps,
+        OutlinedProps,
+        DisabledProps,
+        ShiftProps,
+        AsProps {}
--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,131 +1,20 @@
-import React from 'react';
 import styled from 'styled-components';
 import { Button as BaseButton, ButtonProps as BaseButtonProps } from '@sberdevices/plasma-core/components/Button';
-import type { AsProps } from '@sberdevices/plasma-core/types';
 
 import { applyInteraction, InteractionProps } from '../../mixins';
 
-/**
- * С текстом и/или контентом слева.
- */
-interface TextAndLeftProps {
-    /**
-     * Текстовая надпись на кнопке
-     */
-    text: string | number;
-    /**
-     * Кастомный контент кнопки. При указании этого свойства contentLeft, contentRight и text не применяются
-     */
-    children?: never;
-    /**
-     * Слот для контента слева, например Icon
-     */
-    contentLeft?: React.ReactNode;
-}
-
-/**
- * С текстом и/или контентом справа.
- */
-interface TextAndRightProps {
-    text: string | number;
-    children?: never;
-    /**
-     * Слот для контента справа, например Icon
-     */
-    contentRight?: React.ReactNode;
-}
-
-/**
- * С контентом слева.
- */
-interface LeftProps {
-    children?: never;
-    contentLeft: React.ReactNode;
-}
-
-/**
- * Через ``children``.
- */
-interface ChildrenProps {
-    children: React.ReactNode;
-}
-
-/**
- * Интерфейс кнопки.
- */
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
-    AsProps &
-    Partial<BaseButtonProps> &
-    Pick<InteractionProps, 'scaleOnInteraction'> &
-    (TextAndLeftProps | TextAndRightProps | LeftProps | ChildrenProps) & {
-        size?: 'l' | 'm' | 's';
-    };
-
-interface StyledTextProps {
-    isContentLeft?: boolean;
-    isContentRight?: boolean;
-}
-
-const StyledText = styled.span<StyledTextProps>`
-    box-sizing: border-box;
-
-    ${({ isContentLeft }) => isContentLeft && 'margin-left: 0.375rem;'}
-    ${({ isContentRight }) => isContentRight && 'margin-right: 0.375rem;'}
-`;
-
-/**
- * Для внутреннего использования.
- */
-interface AllContentProps
-    extends TextAndLeftProps,
-        Pick<TextAndRightProps, 'contentRight'>,
-        Pick<TextAndRightProps, 'children'> {}
-
-export const StyledBaseButton = styled(BaseButton)`
-    ${applyInteraction}
-`;
+export type ButtonProps = BaseButtonProps<'l' | 'm' | 's'> & InteractionProps;
 
 /**
  * Основной компонент для создания кнопок.
  */
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    // eslint-disable-next-line prefer-arrow-callback
-    function Button(
-        {
-            view = 'secondary',
-            size = 'l',
-            pin = 'square-square',
-            outlined = true,
-            resizible = false,
-            scaleOnInteraction = true,
-            square,
-            ...props
-        },
-        ref,
-    ) {
-        const { text, contentLeft, contentRight, children, ...rest } = props as AllContentProps;
+export const Button = styled(BaseButton)<ButtonProps>`
+    ${applyInteraction}
+`;
 
-        return (
-            <StyledBaseButton
-                view={view}
-                size={size}
-                pin={pin}
-                outlined={outlined}
-                square={square !== undefined ? square : !text && !children}
-                resizible={resizible}
-                scaleOnInteraction={scaleOnInteraction}
-                ref={ref}
-                {...rest}
-            >
-                {children}
-                {!children && contentLeft}
-                {!children && text && (
-                    <StyledText isContentLeft={!!contentLeft} isContentRight={!!contentRight}>
-                        {text}
-                    </StyledText>
-                )}
-                {!children && contentRight}
-            </StyledBaseButton>
-        );
-    },
-);
+Button.defaultProps = {
+    ...BaseButton.defaultProps,
+    size: 'l',
+    outlined: true,
+    scaleOnInteraction: true,
+};
