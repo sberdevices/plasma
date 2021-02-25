64dc1d6607b6afc2602a3bb6c0f853ca82e065d2
Konstantin Polyanskiy <kvpolyanskiy@sberbank.ru>
2021-02-25 18:35:39 +0300

    fix(plasma-template): set default focus on gallery card

--- packages/plasma-temple/src/templates/Gallery/Gallery.tsx
+++ packages/plasma-temple/src/templates/Gallery/Gallery.tsx
@@ -1,76 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-import { CardBadge, CardBody1, CardMedia, CardBody, CardContent, Card } from '@sberdevices/ui/components/Card';
-import { Carousel, CarouselCol, CarouselGridWrapper } from '@sberdevices/ui/components/Carousel';
+import { Carousel, CarouselGridWrapper } from '@sberdevices/ui/components/Carousel';
 import { Row } from '@sberdevices/ui/components/Grid';
 import { isSberPortal } from '@sberdevices/ui/utils';
-import { Headline3, Caption } from '@sberdevices/ui/components/Typography';
-import { overlay, primary } from '@sberdevices/plasma-tokens';
-import { IconClock } from '@sberdevices/plasma-icons/Icons/IconClock';
+import { Headline3 } from '@sberdevices/ui/components/Typography';
 
 import { useAssistantState } from '../../hooks/useAssistantState';
 import { setPositionAction } from '../../store/actions';
 import { PageProps, GalleryViewPayload, GalleryItemViewPayload, Screen } from '../../types';
 import { useActiveElementClick } from '../../hooks/useSpatNav';
 import { Header } from '../../components/Header/Header';
-
-const StyledCardIndex = styled(CardBadge)`
-    position: absolute;
-    top: 16px;
-    left: 16px;
-`;
-
-const StyledCardContent = styled(CardContent)`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-
-    min-height: 200px;
-`;
-
-const StyledTag = styled(CardBadge)`
-    position: absolute;
-    bottom: 16px;
-    left: 16px;
-    padding: 6px 14px;
-
-    padding: 4px 12px;
-    font-weight: 500;
-    font-size: 20px;
-    text-transform: none;
-    line-height: 20px;
-
-    border-radius: 12px;
-    background-color: ${overlay};
-    backdrop-filter: blur(10px);
-`;
+import { GalleryCard } from './components/GalleryCard/GalleryCard';
 
 const StyledRow = styled(Row)`
     padding: 1rem 0;
     box-sizing: border-box;
 `;
-
-const StyledAdditionalContent = styled.div`
-    display: flex;
-    align-items: center;
-
-    margin-top: auto;
-
-    opacity: 0.47;
-    color: ${primary};
-`;
-
-const StyledCaption = styled(Caption)`
-    margin-bottom: 0;
-    margin-left: 4px;
-`;
-
-const Time: React.FC<{ time: string }> = ({ time }) => (
-    <>
-        <IconClock size="xs" />
-        <StyledCaption>{time}</StyledCaption>
-    </>
-);
 
 export const Gallery: React.FC<PageProps<GalleryViewPayload>> = ({
     data,
@@ -133,32 +78,15 @@ export const Gallery: React.FC<PageProps<GalleryViewPayload>> = ({
                 const imageSrc = Array.isArray(item.image.src) ? item.image.src[0] : item.image.src;
 
                 return (
-                    <CarouselCol sizeXL={3} sizeL={2} sizeM={2}>
-                        <Card
-                            focused={position === index}
-                            tabIndex={0}
-                            onClick={() => onClickHandler(item)}
-                            onFocus={() => onFocusHandler(index)}
-                            data-cy={`gallery-card-${index}`}
-                        >
-                            <CardBody>
-                                <CardMedia src={imageSrc} ratio={item.image.ratio ?? '1:1'} data-cy="gallery-card-media">
-                                    {item.position && (
-                                        <StyledCardIndex view="index" size="l" circled text={String(item.position)} />
-                                    )}
-                                    {typeof item.tag === 'string' && (
-                                        <StyledTag view="secondary" size="s" text={item.tag} />
-                                    )}
-                                </CardMedia>
-                                <StyledCardContent>
-                                    <CardBody1 lines={2}>{item.label}</CardBody1>
-                                    <StyledAdditionalContent>
-                                        {item.time != null && <Time time={item.time} />}
-                                    </StyledAdditionalContent>
-                                </StyledCardContent>
-                            </CardBody>
-                        </Card>
-                    </CarouselCol>
+                    <GalleryCard
+                        key={item.id}
+                        card={item}
+                        position={position}
+                        index={index}
+                        imageSrc={imageSrc}
+                        onClick={onClickHandler}
+                        onFocus={onFocusHandler}
+                    />
                 );
             },
         );
