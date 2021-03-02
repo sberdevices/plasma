bae0903a53238d8d19b196ac3892d2313ffc91c7
Fanil Zubairov <fannisco@gmail.com>
2021-03-02 16:38:17 +0200

    docs: Дополнена документация по миксинам

---
+++ packages/ui/src/mixins/mixins.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import type {
+    FocusProps,
+    OutlinedProps,
+    BlurProps,
+    DisabledProps,
+    RoundnessProps,
+    ViewProps,
+} from '@sberdevices/plasma-core/mixins';
+
+import { InteractionProps } from '.';
+
+export const OutlinedComponent: React.FC<FocusProps & OutlinedProps> = () => undefined;
+export const BlurredComponent: React.FC<BlurProps> = () => undefined;
+export const DisabledComponent: React.FC<DisabledProps> = () => undefined;
+export const InteractionComponent: React.FC<InteractionProps> = () => undefined;
+export const RoundnessComponent: React.FC<RoundnessProps> = () => undefined;
+export const ViewComponent: React.FC<ViewProps> = () => undefined;
