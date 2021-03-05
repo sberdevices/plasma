3ff845d47a8b6b13c0b864be7d7807e0880e97aa
Konstantin Polyanskiy <kvpolyanskiy@sberbank.ru>
2021-03-05 11:57:52 +0300

    feat(plasma-template): make id required only for multi gallery

--- packages/plasma-temple/src/components/Header/Header.tsx
+++ packages/plasma-temple/src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { mediaQuery } from '@sberdevices/ui/utils';
 import { Header as UIKitHeader } from '@sberdevices/ui/components/Header/Header';
 
-export const headerPaddingYM = 1.125;
-
 export const Header = styled(UIKitHeader)`
-    ${mediaQuery('M')(css`
-        padding-top: ${headerPaddingYM}rem;
-        padding-bottom: ${headerPaddingYM}rem;
-    `)}
+    ${mediaQuery('M')`
+        padding-top: 1.125rem;
+        padding-bottom: 1.125rem;
+    `}
 `;
