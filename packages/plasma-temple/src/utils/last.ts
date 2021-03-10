5529de423c06b0edf91c488b94357d390538be67
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-03-10 18:38:43 +0300

    feat(plasma-temple): add unit tests

--- packages/plasma-temple/src/utils/last.ts
+++ packages/plasma-temple/src/utils/last.ts
@@ -1,7 +1,3 @@
 export const last = <T>(array: T[]): T => array[array.length - 1];
 
-export const replaceLast = <T>(array: T[], lastItem: T): T[] => {
-    array[array.length - 1] = lastItem;
-
-    return array;
-};
+export const replaceLast = <T>(array: T[], lastItem: T): T[] => array.slice(0, -1).concat(lastItem);
