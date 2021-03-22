d448f725d924ebb1c144efc8638000eeccf6a2fb
KateKate <anishkina@gmail.com>
2021-03-22 14:32:36 +0300

    feat: esm build core, icons, ui, web

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import path from 'path';
+
+const outDir = 'es';
+export default {
+    input: 'src/index.ts',
+    treeshake: {
+        propertyReadSideEffects: false,
+    },
+    output: {
+        dir: outDir,
+        format: 'es',
+        freeze: false,
+        esModule: true,
+        sourcemap: true,
+        exports: 'named',
+    },
+    external: (id) => {
+        if (id.startsWith('regenerator-runtime') || id === 'tslib') {
+            return false;
+        }
+        return !id.startsWith('.') && !path.isAbsolute(id);
+    },
+    plugins: [
+        nodeResolve(),
+        typescript({ outDir, declaration: false, declarationMap: false, module: 'esnext' }),
+        getBabelOutputPlugin({ plugins: ['babel-plugin-annotate-pure-calls'] }),
+    ],
+};
