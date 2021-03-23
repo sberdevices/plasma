308cde0d60d637e9c4b9f261f66d0e1ced31d40b
Konstantin Polyanskiy <kvpolyanskiy@sberbank.ru>
2021-03-23 18:32:37 +0300

    feat(plasma-templlate): Video page

---
+++ packages/plasma-temple/src/templates/Video/Video.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { VideoPlayer } from '../../components/VideoPlayer/VideoPlayer';
+import { setPositionAction } from '../../store/actions';
+import { PageProps, VideoViewPayload, Screen } from '../../types';
+import { CanvasAppContext } from '../../canvasAppContext';
+
+export const StyledWrapper = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    margin-left: -50vw;
+    left: 50%;
+`;
+
+export const Video: React.FC<PageProps<VideoViewPayload>> = ({ data, position, uiState, dispatch }) => {
+    const { configRoute } = React.useContext(CanvasAppContext);
+    const  renderCustomControls = configRoute?.type === Screen.video
+        ? configRoute?.renderCustomControls
+        : null;
+
+    const { items, autoPlay, visibleControlList, alwaysShowControls } = data;
+    const videos = Array.isArray(items) ? items : [items];
+    const { src, title, startTime, endTime, poster } = videos[position];
+
+    const onNext = React.useCallback(() => {
+        if (position < videos.length - 1) {
+            dispatch(setPositionAction({ position: position + 1 }));
+        }
+    }, [dispatch, position]);
+
+    const onBack = React.useCallback(() => {
+        if (position > 0) {
+            dispatch(setPositionAction({ position: position - 1 }));
+        } else {
+            window.history.back();
+        }
+    }, [dispatch, position]);
+
+    return (
+        <StyledWrapper>
+            <VideoPlayer
+                src={src}
+                poster={poster}
+                header={title}
+                autoPlay={autoPlay}
+                visibleControlList={visibleControlList}
+                alwaysShowControls={alwaysShowControls}
+                startTime={startTime}
+                endTime={endTime}
+                goNext={onNext}
+                goBack={onBack}
+                nextDisabled={position === videos.length - 1}
+                insets={uiState.insets}
+            >
+                {renderCustomControls}
+            </VideoPlayer>
+        </StyledWrapper>
+    );
+};
+
+export default Video;
