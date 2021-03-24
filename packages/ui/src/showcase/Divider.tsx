f3b9f2397cdec8d6821a9841b18c5c4b44037791
Fanil Zubairov <fannisco@gmail.com>
2021-03-24 17:29:26 +0200

    docs: Showcase/Mobile перенесен в docs/showcase

--- packages/ui/src/showcase/Divider.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { surfaceLiquid03 } from '@sberdevices/plasma-tokens';
-
-export const Divider = styled.div`
-    width: 100%;
-    height: 2px;
-    background: ${surfaceLiquid03};
-`;
