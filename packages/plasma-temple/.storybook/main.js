fd8c21340ccdbd9225188d29f572a0a9b5dd6f86
Vasiliy Loginevskiy <yeti.or@gmail.com>
2021-03-25 09:02:47 +0300

    docs(plasma-temple): add storybook configs

---
+++ packages/plasma-temple/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stories: ['../src/**/*.stories.tsx'],
+    addons: [
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-backgrounds/register',
+        '@storybook/addon-viewport/register',
+        '@storybook/addon-actions',
+    ],
+};
