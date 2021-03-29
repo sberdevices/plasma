4c23279f4578e3087d31164ad73f035ce081fdad
kvpolyanskiy <kv.polyanskiy@gmail.com>
2021-03-29 12:35:43 +0300

    fix(plasma-template): Get rid of header animation if logo is absent. Add header back button

--- packages/plasma-temple/src/components/Header/Header.tsx
+++ packages/plasma-temple/src/components/Header/Header.tsx
@@ -1,10 +1,27 @@
+import React from 'react';
 import styled from 'styled-components';
-import { mediaQuery } from '@sberdevices/ui/utils';
+import { isSberBox, mediaQuery } from '@sberdevices/ui/utils';
 import { Header as UIKitHeader } from '@sberdevices/ui/components/Header/Header';
+import { HeaderPropsPayload } from '../../types';
 
-export const Header = styled(UIKitHeader)`
+const StyledHeader = styled(UIKitHeader)`
     ${mediaQuery('M')`
         padding-top: 1.125rem;
         padding-bottom: 1.125rem;
     `}
 `;
+
+interface HeaderProps extends HeaderPropsPayload {
+    back?: boolean;
+}
+
+export const Header = (props: HeaderProps) => {
+    const backProps = React.useMemo(() => isSberBox()
+        ? {}
+        : {
+            back: true,
+            onBackClick: () => window.history.back(),
+        }, []);
+
+    return <StyledHeader {...backProps} {...props} />
+}
