e649f9a24a363d3014499f4df5fdfb22c4508c4c
kvpolyanskiy <kv.polyanskiy@gmail.com>
2021-03-31 18:28:52 +0300

    feat(plasma-template): Improve video player customization. Fix small bugs

---
+++ packages/plasma-temple/src/components/MediaPlayer/index.ts
@@ -0,0 +1,4 @@
+export { MediaPlayerControls } from './MediaPlayerControls';
+export { MediaPlayerButton } from './MediaPlayerButton';
+export { MediaPlayerTimeline, formatTime } from './MediaPlayerTimeline';
+export * from './types';
