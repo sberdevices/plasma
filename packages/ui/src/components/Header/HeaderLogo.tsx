606cd46c6e78114028a6692f8d51578863637244
Vasiliy Loginevskiy <yeti.or@gmail.com>
2021-04-02 12:37:41 +0300

    fix(ui): Header: narrow props

--- packages/ui/src/components/Header/HeaderLogo.tsx
+++ packages/ui/src/components/Header/HeaderLogo.tsx
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 import { Image, ImageProps } from '../Image';
 
-export type HeaderLogoProps = ImageProps;
+export type HeaderLogoProps = Omit<ImageProps, 'height' | 'ratio' | 'customRatio'>;
 
 /**
  * Компонент для размещения логотипа.
  */
-export const HeaderLogo = styled(Image)`
+export const HeaderLogo = styled(Image)<HeaderLogoProps>`
     width: var(--plasma-header-height);
     height: var(--plasma-header-height);
     flex: 0 0 var(--plasma-header-height);
