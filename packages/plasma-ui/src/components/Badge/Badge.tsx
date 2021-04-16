d2d017dc5acb38161670e9d250060706cc4b0ebf
Fanil Zubairov <fannisco@gmail.com>
2021-04-16 14:10:17 +0200

    fix(plasma-ui, plasma-web): Badge: correct reexport & const creation

---
+++ packages/plasma-ui/src/components/Badge/Badge.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Badge as BaseBadge } from '@sberdevices/plasma-core/components/Badge';
+
+/**
+ * Небольшая бирка для ячеек и карточек.
+ * Компонент может отображаться в нескольких размерах и цветах, может содержать текст и/или иконку.
+ */
+export const Badge = styled(BaseBadge)``;
--- packages/plasma-ui/src/components/Badge/index.ts
+++ packages/plasma-ui/src/components/Badge/index.ts
@@ -1,2 +1,4 @@
-export { Badge, badgeSizes, badgeRootSizes, badgeViews } from '@sberdevices/plasma-core/components/Badge';
+export { badgeSizes, badgeRootSizes, badgeViews } from '@sberdevices/plasma-core/components/Badge';
 export type { BadgeProps, BadgeSize, BadgeView } from '@sberdevices/plasma-core/components/Badge';
+
+export { Badge } from './Badge';
