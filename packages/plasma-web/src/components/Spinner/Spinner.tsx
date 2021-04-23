ae146b1b37ef40c1a25b3f0f45f2c64bf9bbd0e8
Viktor Prokopyev <VSProkopyev@sberbank.ru>
2021-04-23 08:23:02 +0300

    feat(plasma-web): Added Spinner to web

---
+++ packages/plasma-web/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Spinner as SpinnerBase, SpinnerProps as SpinnerPropsBase } from '@sberdevices/plasma-core';
+
+export interface SpinnerProps extends SpinnerPropsBase {}
+
+/**
+ * Компонент для отображения индикатора загрузки.
+ */
+export const Spinner = styled(SpinnerBase)``;
