eeaaa79d9d9e023acb353c8f531e7f764853b44e
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-05-13 19:11:19 +0300

    feat(plasma-temple): Confirm Order page

---
+++ packages/plasma-temple/src/pages/ConfirmOrder/ConfirmOrder.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@sberdevices/plasma-ui';
+import { IconEdit } from '@sberdevices/plasma-icons';
+
+import { ConfirmOrder } from './ConfirmOrder';
+
+export default {
+    title: 'Pages/Confirm Order',
+};
+
+const delivery = {
+    details: 'До двери, СДЭК',
+    amount: 500,
+};
+
+const recipient = {
+    name: 'Константин Константинов',
+    phone: '+7 910 345-87-67',
+    email: 'Kostyan3000@gmail.com',
+};
+const address = {
+    title: 'Кутузовский проспект, 32',
+    content: 'Квартира 48, подъезд 1, этаж 6, домофон 48',
+};
+
+export const Default = (): React.ReactElement => {
+    return (
+        <ConfirmOrder
+            delivery={delivery}
+            recipient={recipient}
+            address={address}
+            amount={10456}
+            onPay={action('onPay')}
+            header={{
+                title: 'Оформление заказа',
+                children: (
+                    <Button
+                        text="Изменить адрес"
+                        view="clear"
+                        contentLeft={<IconEdit />}
+                        onClick={action('onChangeAddress')}
+                    />
+                ),
+            }}
+        />
+    );
+};
--- packages/plasma-temple/src/pages/index.ts
+++ packages/plasma-temple/src/pages/index.ts
@@ -2,3 +2,4 @@ export * from './GalleryPage';
 export * from './ItemPage';
 export * from './VideoPage';
 export * from './OrderSuccess';
+export * from './ConfirmOrder';
