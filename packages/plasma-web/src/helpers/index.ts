12a340c57a900e9432c189f13a2107fd9a5a9f6d
Fanil Zubairov <fannisco@gmail.com>
2021-05-18 17:05:09 +0200

    feat: Added plasma-colors package

--- packages/plasma-web/src/helpers/index.ts
+++ packages/plasma-web/src/helpers/index.ts
@@ -1,2 +1,3 @@
 export { IconPlaceholder } from './IconPlaceholder';
 export { InSpacing as InSpacingDecorator } from './StoryDecorators';
+export { PaletteGrid, flattenPalette } from './PaletteGrid';
