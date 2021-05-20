8c58cc7625c3a4a98ad3e17d7f97bbf1c263528a
VladislavPetyukevich <pet.vladislav@gmail.com>
2021-05-20 16:25:03 +0300

    test: fix jest-styled-components import

--- packages/plasma-ui/src/components/Button/Button.test.tsx
+++ packages/plasma-ui/src/components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import _ from 'jest-styled-components';
+import 'jest-styled-components';
 import {
     background,
     blackSecondary,
