8f68cb576300c1b0fb84da1cdcc18a239dd37041
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-05-26 11:39:05 +0300

    chore(plasma-temple): assistant-client up version
    fix(plasma-temple): assistant actions typings

--- packages/plasma-temple/src/utils/createAction.ts
+++ packages/plasma-temple/src/utils/createAction.ts
@@ -1,13 +1,11 @@
-export type ActionTypes<T> = T extends { [key: string]: infer U } ? U : never;
-
-export interface EmptyAction<T extends string> {
+export type EmptyAction<T extends string> = {
     type: T;
-}
+};
 
-export interface Action<T extends string, P> {
+export type Action<T extends string, P> = {
     type: T;
     payload: P;
-}
+};
 
 function inferLiteral<T extends string>(arg: T): T {
     return arg;
