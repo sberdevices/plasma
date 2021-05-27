5ad310e7d22b6cd254e1d6d25e80854e891007d9
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-05-27 16:19:28 +0300

    refactor(plasma-temple): Rename ConfirmOrder and OrderSuccess

--- packages/plasma-temple/src/pages/OrderSuccessPage/OrderSuccessPage.stories.tsx
+++ packages/plasma-temple/src/pages/OrderSuccessPage/OrderSuccessPage.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { HeaderProps } from '@sberdevices/plasma-ui/components/Header/Header';
 
-import { OrderSuccess } from './OrderSuccess';
+import { OrderSuccessPage } from './OrderSuccessPage';
 
 export default {
     title: 'Pages/Order Success',
@@ -17,5 +17,5 @@ const imageSrc =
     'https://s3-alpha-sig.figma.com/img/e31a/3985/e296c0fe873f8d9dcb30b16f665c9f08?Expires=1621814400&Signature=M28Ip1JSifr8TulGdTZUEGaO3s561CdHYlheE5y26tcCNoRsc~b5zoqwrwMEewte8UF8HH-c~BCkM7~WNQHlPQkTlG-b3Mc9lTz6xeEFQlZz7kN0ByX2vi6ENS1UiSS0-1uAAW0uBZCBCODkW~0S50loW8MUjzK3PTHCZFPpt3ZxFR3W44un8uiI3BM0FzL2KKOFeal4RVUASQTsJ3371PEiWztYgd~s5Um4E20LEVCSfwMCLEYTqkQD1XtDyM~v0ByShfBTWSJkfPJH~cjUhK8hRbYjo8JnV2TfcfHnoqm6OXBs6ErWE9Nb-nq5mHKpgKq7SiMTU-o-uhaLr0xK6g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA';
 
 export const Default = (): React.ReactElement => {
-    return <OrderSuccess header={header} imageSrc={imageSrc} onGoBack={action('onGoBack')} />;
+    return <OrderSuccessPage header={header} imageSrc={imageSrc} onGoBack={action('onGoBack')} />;
 };
