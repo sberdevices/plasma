f48fa7b0660327531b80ba91c425a1b51b421e5b
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-05-28 17:13:55 +0300

    feat(plasma-temple): Cart bubble

--- packages/plasma-temple/src/pages/CartPage/components/CartButton/CartButton.stories.tsx
+++ packages/plasma-temple/src/pages/CartPage/components/CartButton/CartButton.stories.tsx
@@ -1,13 +1,76 @@
 import React from 'react';
 
+import { CartItem } from '../../types';
+import { CartProvider } from '../CartProvider/CartProvider';
+
 import { CartButton } from './CartButton';
 
 export default {
     title: 'Cart Button',
 };
 
+const items: CartItem[] = [
+    {
+        id: '1',
+        name: 'Молоко Parmalat ультрапастеризованное dfdfdfdfss',
+        nameDetails: '1л',
+        price: 68,
+        quantity: 9,
+    },
+    {
+        id: '2',
+        name: 'CCC 3x3x3 Sail W',
+        price: 68,
+        quantity: 2,
+        imageSrc:
+            'https://s3-alpha-sig.figma.com/img/3c44/70e1/c35afe61e8fa34bc63b982b455c58c1e?Expires=1622419200&Signature=LBFscWR-5zOwbABoL8bhJOdapHhsB~Sti3~4YkBYWE~GEJ57OEzCd~W9ZC1VAHbqTEasSSx8QvEoqqpxdpTOG6dg7LU9VybjQbny9yfI6FTusoz9ZsPBAG9c4H6AWuCodIqcziZdNPpPf3PSt7X545YUEYUNXR~OCfWt3P5MzOYHq4Gm-RFiQ1gPmwLO4RArxdNnfZpQSMa5RV~ogjoLQwb7f8kFlEbfex6YqgqYWYrL2Nev4F~9ZJpLvSTn8fzihWpW2K3RLn8sQFe6iOcDzl5fSsGmk7-L2Q~1CNkMDWjWMf0vwUixg6hb8EZRUrKboN80lVaEfrk3cVC8IOaEdA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+    },
+    {
+        id: '3',
+        name: 'Молоко Parmalat ультрапастеризованное',
+        nameDetails: '1л',
+        price: 68,
+        quantity: 2,
+    },
+    {
+        id: '4',
+        name: 'CCC 3x3x3 Sail W',
+        price: 68,
+        quantity: 2,
+        imageSrc:
+            'https://s3-alpha-sig.figma.com/img/3c44/70e1/c35afe61e8fa34bc63b982b455c58c1e?Expires=1622419200&Signature=LBFscWR-5zOwbABoL8bhJOdapHhsB~Sti3~4YkBYWE~GEJ57OEzCd~W9ZC1VAHbqTEasSSx8QvEoqqpxdpTOG6dg7LU9VybjQbny9yfI6FTusoz9ZsPBAG9c4H6AWuCodIqcziZdNPpPf3PSt7X545YUEYUNXR~OCfWt3P5MzOYHq4Gm-RFiQ1gPmwLO4RArxdNnfZpQSMa5RV~ogjoLQwb7f8kFlEbfex6YqgqYWYrL2Nev4F~9ZJpLvSTn8fzihWpW2K3RLn8sQFe6iOcDzl5fSsGmk7-L2Q~1CNkMDWjWMf0vwUixg6hb8EZRUrKboN80lVaEfrk3cVC8IOaEdA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+    },
+    {
+        id: '5',
+        name: 'Молоко Parmalat ультрапастеризованное',
+        nameDetails: '1л',
+        price: 68,
+        quantity: 2,
+    },
+    {
+        id: '6',
+        name: 'CCC 3x3x3 Sail W',
+        price: 68,
+        quantity: 2,
+        imageSrc:
+            'https://s3-alpha-sig.figma.com/img/3c44/70e1/c35afe61e8fa34bc63b982b455c58c1e?Expires=1622419200&Signature=LBFscWR-5zOwbABoL8bhJOdapHhsB~Sti3~4YkBYWE~GEJ57OEzCd~W9ZC1VAHbqTEasSSx8QvEoqqpxdpTOG6dg7LU9VybjQbny9yfI6FTusoz9ZsPBAG9c4H6AWuCodIqcziZdNPpPf3PSt7X545YUEYUNXR~OCfWt3P5MzOYHq4Gm-RFiQ1gPmwLO4RArxdNnfZpQSMa5RV~ogjoLQwb7f8kFlEbfex6YqgqYWYrL2Nev4F~9ZJpLvSTn8fzihWpW2K3RLn8sQFe6iOcDzl5fSsGmk7-L2Q~1CNkMDWjWMf0vwUixg6hb8EZRUrKboN80lVaEfrk3cVC8IOaEdA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+    },
+];
+
 export const Default = () => (
-    <div>
+    <CartProvider defaultCartItems={items}>
         <CartButton screen="cart" />
-    </div>
+    </CartProvider>
+);
+
+export const EmptyCart = () => (
+    <CartProvider>
+        <CartButton screen="cart" />
+    </CartProvider>
+);
+
+export const WithPrice = () => (
+    <CartProvider defaultCartItems={items}>
+        <CartButton screen="cart" withPrice />
+    </CartProvider>
 );
--- packages/plasma-temple/src/pages/CartPage/components/CartButton/CartButton.tsx
+++ packages/plasma-temple/src/pages/CartPage/components/CartButton/CartButton.tsx
@@ -1,16 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 import { IconCart } from '@sberdevices/plasma-icons';
-import { Button } from '@sberdevices/plasma-ui';
+import { Badge, Button, Price } from '@sberdevices/plasma-ui';
 
 import { AppStateContext } from '../../../../components/PlasmaApp/AppStateContext';
+import { useCart } from '../../hooks';
 
 interface CartButtonProps {
     screen: string;
+    withPrice?: boolean;
 }
 
-export const CartButton: React.FC<CartButtonProps> = ({ screen }) => {
+const StyledBadge = styled(Badge)`
+    position: absolute;
+    left: 0.75rem;
+    top: -8px;
+`;
+
+const StyledIconCount = styled.div`
+    position: relative;
+`;
+
+export const CartButton: React.FC<CartButtonProps> = ({ screen, withPrice }) => {
     const { pushScreen } = React.useContext(AppStateContext);
+    const { items, currency } = useCart();
+
     const onClick = React.useCallback(() => pushScreen(screen, null), [screen, pushScreen]);
 
-    return <Button contentLeft={<IconCart />} text="Корзина" view="clear" size="s" onClick={onClick} />;
+    const { count, price } = items.reduce(
+        (acc, item) => ({ count: acc.count + item.quantity, price: acc.price + item.price * item.quantity }),
+        { count: 0, price: 0 },
+    );
+
+    return (
+        <Button
+            contentLeft={
+                <StyledIconCount>
+                    {count > 0 && <StyledBadge text={String(count)} size="s" view="primary" circled />}
+                    <IconCart />
+                </StyledIconCount>
+            }
+            text={withPrice && price > 0 ? <Price currency={currency}>{price}</Price> : 'Корзина'}
+            view="clear"
+            size="s"
+            onClick={onClick}
+        />
+    );
 };
