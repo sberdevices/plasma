7ad3b32d29ee8ab5c786420b9e8c31ad45f19920
Fanil Zubairov <fannisco@gmail.com>
2021-06-07 13:53:16 +0200

    docs(plasma-web): Added themes story

--- packages/plasma-web/src/components/Colors.stories.tsx
+++ packages/plasma-web/src/components/Colors.stories.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 import { general, additional } from '@sberdevices/plasma-colors';
 
-import { InSpacingDecorator, PaletteGrid, flattenPalette } from '../helpers';
+import { InSpacingDecorator, ThemeColors, ThemeBG, PaletteGrid, flattenPalette } from '../helpers';
 
 export default {
     title: 'Colors',
     decorators: [InSpacingDecorator],
 };
 
+const darkThemes = ['dark'];
+const lightThemes = ['light'];
+
+const StyledContainer = styled.div`
+    display: flex;
+`;
+
+export const Default = () => {
+    return (
+        <StyledContainer>
+            <ThemeBG mode="light">
+                {lightThemes.map((theme, i) => (
+                    <>
+                        <ThemeColors
+                            key={`item:${i}`}
+                            theme={theme as 'light'}
+                            title={i === 0 ? '🌝 Light Theme Colors' : ''}
+                            heading={theme}
+                        />
+                    </>
+                ))}
+            </ThemeBG>
+            <ThemeBG mode="dark">
+                {darkThemes.map((theme, i) => (
+                    <>
+                        <ThemeColors
+                            key={`item:${i}`}
+                            theme={theme as 'dark'}
+                            title={i === 0 ? '🌚 Dark Theme Colors' : ''}
+                            heading={theme}
+                        />
+                    </>
+                ))}
+            </ThemeBG>
+        </StyledContainer>
+    );
+};
+
 const generalColors = flattenPalette(general);
-const additionalColors = flattenPalette((additional as unknown) as typeof general);
+const additionalColors = flattenPalette(additional);
 
 export const General = () => <PaletteGrid colors={generalColors} />;
 export const Additional = () => <PaletteGrid colors={additionalColors} />;
