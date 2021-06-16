30779982d1eae9d45253ae796b0cd39afdde8f58
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-06-16 14:34:33 +0300

    feat(plasma-temple): Get rid of first gallery scroll

--- packages/plasma-temple/src/pages/GalleryPage/components/Carousel.ts
+++
@@ -1,9 +0,0 @@
-import { Carousel, CarouselProps as PlasmaCarouselProps } from '@sberdevices/plasma-ui';
-import styled from 'styled-components';
-
-export type CarouselProps = Pick<PlasmaCarouselProps, 'axis' | 'onIndexChange' | 'index'>;
-
-export const CommonCarousel = styled(Carousel)`
-    padding-right: var(--plasma-grid-margin);
-    scroll-behavior: smooth;
-`;
