2493979eab7ce1cf79d1ae8203ac92f5a62c0ffb
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-06-16 16:52:35 +0300

    chore(plasma-temple): apply new component for existing page layout

--- packages/plasma-temple/src/pages/OrderSuccessPage/OrderSuccessPage.tsx
+++ packages/plasma-temple/src/pages/OrderSuccessPage/OrderSuccessPage.tsx
@@ -1,46 +1,21 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-import { Button, Col, Headline1, Headline2, Row, Image } from '@sberdevices/plasma-ui';
+import { Button } from '@sberdevices/plasma-ui';
 import { HeaderProps } from '@sberdevices/plasma-ui/components/Header/Header';
-import { detectDevice, isSberBox, mediaQuery } from '@sberdevices/plasma-ui/utils';
+import { isSberBox } from '@sberdevices/plasma-ui/utils';
 
-import { Header } from '../../components/Header/Header';
-import { DeviceFamily } from '../../types';
 import { useFocusOnMount } from '../../hooks/useFocusOnMount';
 import { THROTTLE_WAIT } from '../../hooks/useThrottledCallback';
+import { useRegistry } from '../../hooks/useRegistry';
+import { ComponentPropsWithHeader } from '../../components/Header/types';
 
-interface OrderSuccessProps {
+interface OrderSuccessProps extends ComponentPropsWithHeader {
     header?: HeaderProps;
     imageSrc?: string;
     onGoBack: () => void;
 }
 
-const mapDeviceToHeadline: Record<DeviceFamily, React.FC> = {
-    sberBox: Headline1,
-    sberPortal: Headline2,
-    mobile: Headline2,
-};
-
-const StyledHeadline = styled(mapDeviceToHeadline[detectDevice()])`
-    margin-top: 1.9rem;
-    margin-bottom: 2rem;
-`;
-
-const StyledImageContainer = styled.div`
-    margin-left: auto;
-    width: 656px;
-
-    ${mediaQuery(
-        'M',
-        2,
-    )(
-        css`
-            width: 408px;
-        `,
-    )}
-`;
-
 export const OrderSuccessPage: React.FC<OrderSuccessProps> = ({ header, imageSrc = '', onGoBack }) => {
+    const { StateLayout } = useRegistry();
     const buttonRef = React.useRef<HTMLButtonElement>(null);
 
     useFocusOnMount<HTMLButtonElement>(buttonRef, {
@@ -49,23 +24,15 @@ export const OrderSuccessPage: React.FC<OrderSuccessProps> = ({ header, imageSrc
     });
 
     return (
-        <>
-            {header && <Header {...header} />}
-            <Row>
-                <Col sizeXL={6} sizeM={3}>
-                    <StyledHeadline>Заказ успешно оформлен! Статус заказа будет отправлен на E-mail</StyledHeadline>
-                    <Button view="primary" onClick={onGoBack} ref={buttonRef}>
-                        Вернуться в магазин
-                    </Button>
-                </Col>
-                {imageSrc && (
-                    <Col sizeXL={6} sizeM={3}>
-                        <StyledImageContainer>
-                            <Image base="div" src={imageSrc} ratio="1 / 1" />
-                        </StyledImageContainer>
-                    </Col>
-                )}
-            </Row>
-        </>
+        <StateLayout
+            header={header}
+            title="Заказ успешно оформлен! Статус заказа будет отправлен на E-mail"
+            backgound={imageSrc}
+            button={
+                <Button view="primary" onClick={onGoBack} ref={buttonRef}>
+                    Вернуться в магазин
+                </Button>
+            }
+        />
     );
 };
