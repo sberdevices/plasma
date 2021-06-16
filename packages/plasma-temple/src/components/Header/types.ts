4ebf0f7b666db8377818833586f69cccb079eac6
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-06-16 16:51:23 +0300

    chore(plasma-temple): utility type for Header component props

---
+++ packages/plasma-temple/src/components/Header/types.ts
@@ -0,0 +1,7 @@
+import { HeaderProps } from '@sberdevices/plasma-ui/components/Header/Header';
+
+export type { HeaderProps };
+
+export interface ComponentPropsWithHeader {
+    header?: HeaderProps;
+}
