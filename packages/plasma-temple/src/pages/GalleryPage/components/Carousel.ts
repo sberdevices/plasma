7a978851511457f4ba1344a26c6cbbdf35af7de6
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-06-18 10:26:28 +0300

    feat(plasma-temple): remove gallery animation for portal

---
+++ packages/plasma-temple/src/pages/GalleryPage/components/Carousel.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Carousel } from '@sberdevices/plasma-ui';
+
+export const CommonCarousel = styled(Carousel)`
+    padding-right: var(--plasma-grid-margin);
+`;
--- packages/plasma-temple/src/pages/GalleryPage/components/Carousel@sberbox.tsx
+++ packages/plasma-temple/src/pages/GalleryPage/components/Carousel@sberbox.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 import { CarouselProps } from '@sberdevices/plasma-ui';
+
+import { useDelayedActivation } from '../../../hooks/useDelayedActivation';
 
 import { CommonCarousel } from './Carousel';
 
-export const Carousel = React.forwardRef<HTMLDivElement, CarouselProps>(({ axis, index, children }, ref) => (
-    <CommonCarousel axis={axis} index={index} paddingEnd="50vh" ref={ref}>
-        {children}
-    </CommonCarousel>
-));
+const StyledCarousel = styled(CommonCarousel)<{ initialized: boolean }>`
+    scroll-behavior: ${({ initialized }) => (initialized ? 'smooth' : 'unset')};
+`;
+
+export const Carousel = React.forwardRef<HTMLDivElement, CarouselProps>(({ axis, index, children }, ref) => {
+    /* Если индекс карусели изначально не равен 0, то выполняется проскролл до нужного элемента
+       в результате происходит не очень красивый эффект автоматического скролла при открытии страницы,
+       т.к. включена анимация скролла. Чтобы избежать данного эффекта, изначально анимация отключена и
+       включается только после не большой задержки.
+       Возможно стоит рассмотреть доработку стандартного компонента карусели, чтобы убрать данный хак.
+    */
+    const initialized = useDelayedActivation();
+
+    return (
+        <StyledCarousel axis={axis} index={index} paddingEnd="50vh" ref={ref} initialized={initialized}>
+            {children}
+        </StyledCarousel>
+    );
+});
