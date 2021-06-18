e183182a7785302189571f3f7a2655068a8d19e9
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-06-18 12:07:57 +0300

    feat(plasma-temple): Posibility to skip assistant native panel insets

--- packages/plasma-temple/src/components/Layout/Layout.tsx
+++ packages/plasma-temple/src/components/Layout/Layout.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled, { CSSObject } from 'styled-components';
 
-import { useInsets } from '../../hooks';
+import { Insets, useInsets } from '../../hooks';
 import { AssistantInsets } from '../../store';
+
+interface LayoutProps {
+    ignoreInsets?: boolean;
+}
 
 interface StyledLayoutProps {
     insets: AssistantInsets;
@@ -23,8 +27,15 @@ export const StyledLayout = styled.div<StyledLayoutProps>`
     ${insetsToCSSObject}
 `;
 
-export const Layout: React.FC = ({ children }) => {
+const defaultInsets: Insets = {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+};
+
+export const Layout: React.FC<LayoutProps> = ({ children, ignoreInsets }) => {
     const insets = useInsets();
 
-    return <StyledLayout insets={insets}>{children}</StyledLayout>;
+    return <StyledLayout insets={ignoreInsets ? defaultInsets : insets}>{children}</StyledLayout>;
 };
