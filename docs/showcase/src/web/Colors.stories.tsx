c1315d32ace4f43d5addd6637ab37f16572dcfe7
Fanil Zubairov <fannisco@gmail.com>
2021-06-18 13:37:57 +0200

    docs: ThemeColors in both libraries

--- docs/showcase/src/web/Colors.stories.tsx
+++ docs/showcase/src/web/Colors.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ThemeColors } from '@sberdevices/plasma-sb-utils';
+import { ThemeColors, extractWebThemeColors } from '@sberdevices/plasma-sb-utils';
 import { light, dark } from '@sberdevices/plasma-tokens-web/themes';
 
 import { WebStoryDecorator } from '../helpers';
@@ -14,19 +14,13 @@ const StyledContainer = styled.div`
     display: flex;
 `;
 
-const extractColors = (theme: Record<string, string>): Record<string, string> =>
-    Object.entries(theme)
-        .filter(([key]) => key.match(/^--/))
-        .reduce((acc, [key, value]) => ({ ...acc, [key.replace('--plasma-colors-', '')]: value }), {});
-
-const lightColors = extractColors(light[':root']);
-const darkColors = extractColors(dark[':root']);
+const colors = extractWebThemeColors(light, dark);
 
 export const Colors = () => {
     return (
         <StyledContainer>
-            <ThemeColors style={{ backgroundColor: '#FAFAFA' }} colors={lightColors} title="🌝 Light Theme Colors" />
-            <ThemeColors style={{ backgroundColor: '#292929' }} colors={darkColors} title="🌚 Dark Theme Colors" />
+            <ThemeColors style={{ backgroundColor: '#FAFAFA' }} colors={colors.light} title="🌝 Light Theme Colors" />
+            <ThemeColors style={{ backgroundColor: '#292929' }} colors={colors.dark} title="🌚 Dark Theme Colors" />
         </StyledContainer>
     );
 };
