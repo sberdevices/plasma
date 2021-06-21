ad5f769f990707f77eb3fe72e98c4acda51696ab
VladislavPetyukevich <pet.vladislav@gmail.com>
2021-06-21 12:57:38 +0300

    feat(plasma-temple): Separate interface for description on ItemPages

--- packages/plasma-temple/src/pages/ItemPage/types.ts
+++ packages/plasma-temple/src/pages/ItemPage/types.ts
@@ -1,4 +1,6 @@
-import { Description, MediaObject, MetaPayload } from '../../types';
+import React from 'react';
+
+import { MediaObject, MetaPayload } from '../../types';
 
 export interface ItemEntity extends MetaPayload {
     label: string;
@@ -7,13 +9,18 @@ export interface ItemEntity extends MetaPayload {
     [key: string]: any;
 }
 
+export interface ItemDescription {
+    title: string | React.ReactElement;
+    content: string | React.ReactElement;
+}
+
 export interface ItemPageState extends MetaPayload {
     id: string;
     title: string;
     subtitle?: string;
     entities: Array<ItemEntity>;
     entitiesTitle: string;
-    description: Description[];
+    description: ItemDescription[];
     actionButtonText: string;
     background?: MediaObject;
 }
