c9d7494046fd431bd93bc2ec5876b62a136e8bfd
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-06-21 15:27:56 +0300

    feat(plasma-temple): Store shop landing page active card index

---
+++ packages/plasma-temple/src/pages/ShopLandingPage/components/Carousel/types.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export interface ShopLandingCarouselProps {
+    index: number;
+    children: React.ReactNode;
+}
--- packages/plasma-temple/src/pages/ShopLandingPage/types.ts
+++ packages/plasma-temple/src/pages/ShopLandingPage/types.ts
@@ -4,4 +4,5 @@ import { AnyObject } from '../../types';
 export interface ShopLandingPageState<T extends AnyObject = AnyObject> {
     catalogImage: string;
     items: GalleryCardParams<T>[];
+    activeCardIndex: number;
 }
