368ddef1e25d0e91485ba28ed3080775133afbc2
Fanil Zubairov <fannisco@gmail.com>
2021-07-08 15:21:43 +0200

    chore: Alias react, react-dom and styled-components in Storybook webpack

--- packages/plasma-ui/.storybook/main.js
+++ packages/plasma-ui/.storybook/main.js
@@ -13,4 +13,17 @@ module.exports = {
         '@storybook/addon-docs',
         '@storybook/addon-toolbars',
     ],
+    webpackFinal: async (config) => {
+        return {
+            ...config,
+            resolve: {
+                ...config.resolve,
+                alias: {
+                    react: path.resolve(__dirname, '../', 'node_modules', 'react'),
+                    'react-dom': path.resolve(__dirname, '../', 'node_modules', 'react-dom'),
+                    'styled-components': path.resolve(__dirname, '../', 'node_modules', 'styled-components'),
+                },
+            },
+        };
+    },
 };
