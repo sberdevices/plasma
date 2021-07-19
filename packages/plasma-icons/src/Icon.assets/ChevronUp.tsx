ce44193b3c8e92dc14726567ba4e261f22a5559d
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2021-07-19 11:10:11 +0300

    fix(plasma-icons): chevron-icons updated

--- packages/plasma-icons/src/Icon.assets/ChevronUp.tsx
+++ packages/plasma-icons/src/Icon.assets/ChevronUp.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { IconProps } from '../IconRoot';
 
 export const ChevronUp: React.FC<IconProps> = (props) => (
-    <svg width="100%" viewBox="0 0 16 16" fill="none" {...props}>
+    <svg width="100%" viewBox="0 0 24 24" fill="none" {...props}>
         <path
             fillRule="evenodd"
             clipRule="evenodd"
-            d="M3.293 10.707a1 1 0 001.414 0l2.586-2.586a1 1 0 011.414 0l2.586 2.586a1 1 0 001.414-1.414l-2.586-2.586a3 3 0 00-4.242 0L3.293 9.293a1 1 0 000 1.414z"
+            d="M20.644 16.651a1.235 1.235 0 01-1.72 0L12 9.874l-6.924 6.777a1.235 1.235 0 01-1.72 0 1.173 1.173 0 010-1.683l7.784-7.62A1.23 1.23 0 0112 7c.322 0 .632.125.86.349l7.784 7.619a1.173 1.173 0 010 1.683z"
             fill="currentColor"
         />
     </svg>
