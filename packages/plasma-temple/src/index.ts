1abd624c385720a51791af1d6337af603afa33f9
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-07-20 17:33:20 +0300

    feat(plasma-temple): Refactor gallery

--- packages/plasma-temple/src/index.ts
+++ packages/plasma-temple/src/index.ts
@@ -5,3 +5,4 @@ export * from './pages';
 export * from './constants';
 export * from './store';
 export { getAssistant } from './assistant';
+export * from './utils';
---
+++ packages/plasma-temple/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { getMediaObjectSrc } from './getMediaObjectSrc';
