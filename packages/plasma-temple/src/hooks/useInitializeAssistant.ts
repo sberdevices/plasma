c269c37c65acf7c3ce0b2dad1c191d05fc35047f
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-07-22 12:32:27 +0300

    fix(plasma-temple): assistantClient can be get outside React

--- packages/plasma-temple/src/hooks/useInitializeAssistant.ts
+++ packages/plasma-temple/src/hooks/useInitializeAssistant.ts
@@ -1,72 +1,10 @@
 import { useMemo, useCallback, useRef } from 'react';
-import {
-    AssistantClientCustomizedCommand,
-    AssistantSmartAppData,
-    createAssistant,
-    createAssistantDev,
-} from '@sberdevices/assistant-client';
+import { AssistantClientCustomizedCommand, AssistantSmartAppData } from '@sberdevices/assistant-client';
 
-import { logger } from '../utils/logger';
-import { AssistantInstance, PickOptional } from '../types';
+import { AssistantInstance } from '../types';
+import { InitializeParams, initializeAssistant } from '../assistant';
 
 import { useMount } from './useMount';
-
-export type AssistantProps = Parameters<typeof createAssistantDev>[0];
-
-export type InitializeParams = PickOptional<
-    AssistantProps,
-    'token' | 'getRecoveryState' | 'url' | 'userChannel' | 'surface'
-> &
-    Pick<AssistantProps, 'initPhrase' | 'nativePanel' | 'getState'>;
-
-export const initializeAssistant = <T extends AssistantSmartAppData>({
-    getState,
-    getRecoveryState,
-    initPhrase,
-    nativePanel,
-    token,
-    url = 'wss://vpstest2.online.sberbank.ru:443/vpsdemo2/',
-    userChannel = 'B2C',
-    surface = 'SBERBOX',
-}: InitializeParams): AssistantInstance => {
-    let assistant: AssistantInstance;
-
-    if (process.env.NODE_ENV === 'development') {
-        const environmentProps = token
-            ? {
-                  userChannel: 'B2C',
-                  surface: 'SBERBOX',
-                  url: 'wss://nlp2vps.online.sberbank.ru/vps/',
-                  settings: {
-                      authConnector: 'developer_portal_jwt',
-                  },
-              }
-            : {
-                  url,
-                  userChannel,
-                  surface,
-              };
-
-        assistant = createAssistantDev<T>({
-            getState: () => {
-                const state = getState();
-                logger('Assistant state', state);
-                return state;
-            },
-            getRecoveryState,
-            initPhrase,
-            nativePanel,
-            token,
-            ...environmentProps,
-        });
-
-        assistant.on('data', (action) => logger('Assistant Action', action));
-    } else {
-        assistant = createAssistant<T>({ getState });
-    }
-
-    return assistant;
-};
 
 export const useInitializeAssistant = <T extends AssistantSmartAppData>({
     assistantParams,
