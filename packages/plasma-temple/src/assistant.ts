18d63305ceaf73bb1bf96433e89c9ceea3a6c270
Maxim Sviridov <maksim.v.sviridov@gmail.com>
2021-07-22 12:32:27 +0300

    fix(plasma-temple): assistantClient can be get outside React

---
+++ packages/plasma-temple/src/assistant.ts
@@ -0,0 +1,63 @@
+import { AssistantSmartAppData, createAssistant, createAssistantDev } from '@sberdevices/assistant-client';
+
+import { AssistantInstance, PickOptional } from './types';
+import { logger } from './utils/logger';
+
+export type AssistantProps = Parameters<typeof createAssistantDev>[0];
+
+export type InitializeParams = PickOptional<
+    AssistantProps,
+    'token' | 'getRecoveryState' | 'url' | 'userChannel' | 'surface'
+> &
+    Pick<AssistantProps, 'initPhrase' | 'nativePanel' | 'getState'>;
+
+let assistant: AssistantInstance;
+
+export const getAssistant = (): AssistantInstance => assistant;
+
+export const initializeAssistant = <T extends AssistantSmartAppData>({
+    getState,
+    getRecoveryState,
+    initPhrase,
+    nativePanel,
+    token,
+    url = 'wss://vpstest2.online.sberbank.ru:443/vpsdemo2/',
+    userChannel = 'B2C',
+    surface = 'SBERBOX',
+}: InitializeParams): AssistantInstance => {
+    if (process.env.NODE_ENV === 'development') {
+        const environmentProps = token
+            ? {
+                  userChannel: 'B2C',
+                  surface: 'SBERBOX',
+                  url: 'wss://nlp2vps.online.sberbank.ru/vps/',
+                  settings: {
+                      authConnector: 'developer_portal_jwt',
+                  },
+              }
+            : {
+                  url,
+                  userChannel,
+                  surface,
+              };
+
+        assistant = createAssistantDev<T>({
+            getState: () => {
+                const state = getState();
+                logger('Assistant state', state);
+                return state;
+            },
+            getRecoveryState,
+            initPhrase,
+            nativePanel,
+            token,
+            ...environmentProps,
+        });
+
+        assistant.on('data', (action) => logger('Assistant Action', action));
+    } else {
+        assistant = createAssistant<T>({ getState });
+    }
+
+    return assistant;
+};
