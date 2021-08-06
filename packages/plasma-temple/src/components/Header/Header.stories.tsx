57ac259e51f28b208bd84dbf8ef80023e3d9a7c9
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-08-06 13:43:26 +0300

    feat(plasma-temple): Add header minimize behavior

--- packages/plasma-temple/src/components/Header/Header.stories.tsx
+++ packages/plasma-temple/src/components/Header/Header.stories.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { withAppState, defaultValue } from '../../../.storybook/decorators/withAppState';
 
 import { Header } from './Header';
 
@@ -9,3 +12,17 @@ export default {
 export const Default = () => {
     return <Header logo="./images/320_320_2.jpg" title="Hello" subtitle="plasma-temple" />;
 };
+
+Default.decorators = [withAppState({ ...defaultValue, popScreen: action('popScreen click') })];
+
+export const WithOneScreenInHistory = () => {
+    return <Header logo="./images/320_320_2.jpg" title="Hello" subtitle="plasma-temple" />;
+};
+
+WithOneScreenInHistory.decorators = [
+    withAppState({
+        ...defaultValue,
+        state: { ...defaultValue.state, history: [{ name: 'name', data: null }] },
+        popScreen: action('popScreen click'),
+    }),
+];
