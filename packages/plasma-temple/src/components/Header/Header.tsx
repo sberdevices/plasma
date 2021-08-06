241aeee53e76242edea1b0f504871e7c7b765de0
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-08-06 13:43:26 +0300

    feat(plasma-temple): Add header minimize behavior

--- packages/plasma-temple/src/components/Header/Header.tsx
+++ packages/plasma-temple/src/components/Header/Header.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { isSberBox } from '@sberdevices/plasma-ui/utils';
 import { Header as UIKitHeader } from '@sberdevices/plasma-ui';
 
 import { HeaderProps } from './types';
+import { useHeaderProps } from './useHeaderProps';
 
 export const Header: React.FC<HeaderProps> = (props) => {
-    const withBackProps =
-        isSberBox() || props.back !== undefined
-            ? props
-            : {
-                  ...props,
-                  back: true as const,
-                  onBackClick: () => window.history.back(),
-              };
+    const headerProps = useHeaderProps(props);
 
-    return <UIKitHeader {...withBackProps} />;
+    return <UIKitHeader {...headerProps} />;
 };
