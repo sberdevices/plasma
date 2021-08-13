ca33bf63f13783196505683fdf09c4514d711444
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-08-13 14:21:23 +0300

    feat(plasma-temple): Make page layout scrollable

--- packages/plasma-temple/src/components/PageLayout/PageLayout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { DeviceThemeProvider, Container } from '@sberdevices/plasma-ui';
-
-export const PageLayout: React.FC = ({ children }) => (
-    <DeviceThemeProvider>
-        <Container>{children}</Container>
-    </DeviceThemeProvider>
-);
