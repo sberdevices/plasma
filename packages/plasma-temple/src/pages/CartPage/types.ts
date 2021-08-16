7a54686afadb2ad76e0a37775b1a8f45d3912dce
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-08-16 12:56:48 +0300

    feat(plasma-temple): simplify types

--- packages/plasma-temple/src/pages/CartPage/types.ts
+++ packages/plasma-temple/src/pages/CartPage/types.ts
@@ -10,8 +10,25 @@ export type CartItem<ID = string, T extends AnyObject = AnyObject> = Entity<ID> 
         quantityLimit?: number;
     };
 
-export type ChangeItemQuantityFn<ID = string> = (id: ID, quantity: number) => void;
-export type ChangeStateFn<ID = string, T extends AnyObject = AnyObject> = (state: CartState<ID, T>) => void;
+export type ChangeStateFn<T extends CartState> = (state: T) => void;
+
+export type OnAddCartItemFn<T extends CartState> = (args: {
+    item: CartStateItem<T>;
+    state: T;
+    changeState: ChangeStateFn<T>;
+}) => void;
+
+export type OnChangeCartItemQuantityFn<T extends CartState> = (args: {
+    item: CartStateItem<T>;
+    state: T;
+    changeState: ChangeStateFn<T>;
+}) => void;
+
+export type OnRemoveCartItemFn<T extends CartState> = (args: {
+    item: CartStateItem<T>;
+    state: T;
+    changeState: ChangeStateFn<T>;
+}) => void;
 
 export interface CartState<ID = string, T extends AnyObject = AnyObject> {
     items: CartItem<ID, T>[];
@@ -23,3 +40,5 @@ export interface CartState<ID = string, T extends AnyObject = AnyObject> {
     deliveryPrice?: number;
     discount?: number;
 }
+
+export type CartStateItem<T extends CartState> = T['items'][0];
