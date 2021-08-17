f87b134b48cd60f734026ea6abf9f7552433db6b
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2021-08-17 23:15:27 +0300

    chore(plasma-ui): Storybook controls for Tabs

--- packages/plasma-ui/src/components/Tabs/Tabs.stories.tsx
+++ packages/plasma-ui/src/components/Tabs/Tabs.stories.tsx
@@ -1,57 +1,106 @@
 import React from 'react';
-import { text, number, select, boolean } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Icon } from '@sberdevices/plasma-icons';
 
-import { InSpacingDecorator } from '../../helpers';
+import { InSpacingDecorator, disableProps } from '../../helpers';
 
-import { Tabs, TabItem } from '.';
-
-export default {
-    title: 'Controls/Tabs',
-    component: Tabs,
-    decorators: [InSpacingDecorator],
-};
+import { Tabs, TabItem, TabsProps } from '.';
 
 const sizeKeys = ['l', 'm', 's'];
 const viewKeys = ['secondary', 'black', 'clear'];
 const icons = ['clock', 'settings', 'house', 'trash'];
 
-export const Default = () => {
-    const items = Array(number('Items', 4)).fill(0);
-    const stretch = boolean('stretch', true);
-    const pilled = boolean('pilled', false);
-    const outlined = boolean('outlined', false);
-    const disabled = boolean('disabled', false);
+const propsToDisable = ['index', 'focused', 'shiftLeft', 'shiftRight', 'scaleOnPress', 'as', 'forwardedAs'];
+
+export default {
+    title: 'Controls/Tabs',
+    component: Tabs,
+    decorators: [InSpacingDecorator],
+    argTypes: {
+        animated: {
+            control: {
+                type: 'boolean',
+            },
+        },
+        size: {
+            control: {
+                type: 'inline-radio',
+                options: sizeKeys,
+            },
+        },
+        view: {
+            control: {
+                type: 'inline-radio',
+                options: viewKeys,
+            },
+        },
+        ...disableProps(propsToDisable),
+    },
+} as Meta;
+
+interface DefaultStoryProps {
+    itemsNumber: number;
+    scaleOnInteraction: boolean;
+    enableContentLeft: boolean;
+    text: string;
+}
+
+export const Default: Story<DefaultStoryProps & TabsProps> = ({
+    itemsNumber,
+    size,
+    view,
+    stretch,
+    pilled,
+    scaleOnInteraction,
+    enableContentLeft,
+    text,
+    disabled,
+    outlined,
+    animated,
+}) => {
+    const items = Array(itemsNumber).fill(0);
     const [index, setIndex] = React.useState(0);
 
     return (
         <Tabs
-            size={select('size', sizeKeys, 'l') as 'l'}
-            view={select('view', viewKeys, 'secondary') as 'secondary'}
+            size={size}
+            view={view}
             stretch={stretch}
             pilled={pilled}
-            scaleOnPress={boolean('scaleOnInteraction', true)}
+            scaleOnPress={scaleOnInteraction}
             outlined={!disabled && outlined}
             disabled={disabled}
             index={index}
-            animated={boolean('animated', true) as true}
+            animated={animated}
         >
             {items.map((_, i) => (
                 <TabItem
                     key={`item:${i}`}
                     isActive={i === index}
                     tabIndex={!disabled ? i : -1}
-                    contentLeft={
-                        boolean('contentLeft', true) && <Icon icon={icons[i % icons.length] as 'clock'} size="s" />
-                    }
+                    contentLeft={enableContentLeft && <Icon icon={icons[i % icons.length] as 'clock'} size="s" />}
                     onClick={() => !disabled && setIndex(i)}
                     onFocus={action(`onFocus item #${i}`)}
                     onBlur={action(`onBlur item #${i}`)}
                 >
-                    {text('Text', 'Label')}
+                    {text}
                 </TabItem>
             ))}
         </Tabs>
     );
+};
+
+Default.args = {
+    itemsNumber: 4,
+    size: 'l',
+    view: 'secondary',
+    stretch: true,
+    pilled: false,
+    scaleOnInteraction: true,
+    outlined: false,
+    disabled: false,
+    animated: true,
+    enableContentLeft: true,
+    text: 'Label',
 };
