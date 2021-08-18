6ea8957b7e53e9b1b57c2ff01ab579c610639853
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2021-08-18 23:17:10 +0300

    chore(plasma-web): disableProps added

--- packages/plasma-web/src/helpers/index.ts
+++ packages/plasma-web/src/helpers/index.ts
@@ -1,3 +1,4 @@
 export { IconPlaceholder } from './IconPlaceholder';
+export { disableProps } from './disableProps';
 export { InSpacing as InSpacingDecorator } from './StoryDecorators';
 export { PaletteGrid, flattenPalette } from './PaletteGrid';
