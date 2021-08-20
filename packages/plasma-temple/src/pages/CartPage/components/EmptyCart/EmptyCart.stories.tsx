edf5507de153467334c1cc590d5cf239583ad31f
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-08-20 14:26:12 +0300

    feat(plasma-temple): Empty cart mobile version

--- packages/plasma-temple/src/pages/CartPage/components/EmptyCart/EmptyCart.stories.tsx
+++ packages/plasma-temple/src/pages/CartPage/components/EmptyCart/EmptyCart.stories.tsx
@@ -7,9 +7,6 @@ export default {
     title: 'Pages/EmptyCart',
 };
 
-const imageSrc =
-    'https://s3-alpha-sig.figma.com/img/e31a/3985/e296c0fe873f8d9dcb30b16f665c9f08?Expires=1623024000&Signature=Hd27xDiwogxvk2mQxI~uCP30MjJTenkv9fegf85Zmfy4vmvvHhXXdBKBoVcDsfZ6iDD3bTB8krgZrZ~C1UDWVwJl3P8DTCYiTVJKvqjqOwyhJkS7EwZ9H2Zn07-zQ~y5rZgKnFHcGRCdwCDTk~rckekP4u-DqyjhejegV9S~XhZY8DmJJlD0RH0uu3ChF4~AC8S-wrlnekiQ45RU~LEodf0R83PPyeGOuU6Rzuj6UsumHemoLOxLj6IitvF~fI7vCGHEn5IijjDCyPPPJl0K6daN5vb4qd1yLa~K03QifmxaVDL8n6sLDWDM4CnQt8S7eR9EMzdi1swOJtIcinH2Bw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA';
-
 export const Default = (): React.ReactElement => {
-    return <EmptyCart imageSrc={imageSrc} onGoToCatalog={action('onGoToCatalog')} />;
+    return <EmptyCart imageSrc="/images/empty-cart.png" onGoToCatalog={action('onGoToCatalog')} />;
 };
---
+++ packages/plasma-temple/src/pages/CartPage/components/EmptyCart/EmptyCart@common.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { Button, Col, Headline1, Headline2, Row, Image, ButtonProps } from '@sberdevices/plasma-ui';
+import { isSberBox, mediaQuery } from '@sberdevices/plasma-ui/utils';
+
+import { deviceFamily } from '../../../../utils/deviceFamily';
+import { DeviceFamily } from '../../../../types';
+import { withProps } from '../../../../components/hocs/withProps';
+import { useFocusOnMount } from '../../../../hooks/useFocusOnMount';
+
+export interface EmptyCartProps {
+    imageSrc?: string;
+    onGoToCatalog?: () => void;
+    buttonText?: string;
+    className?: string;
+}
+
+export const defaultButtonText = 'Вернуться в каталог';
+
+const mapDeviceToTitle: Record<DeviceFamily, React.FC> = {
+    sberBox: Headline1,
+    sberPortal: Headline2,
+    mobile: Headline2,
+};
+
+const StyledTitle = styled(mapDeviceToTitle[deviceFamily])`
+    margin-bottom: 2.5rem;
+
+    ${mediaQuery(
+        'M',
+        2,
+    )(
+        css`
+            margin-bottom: 1.75rem;
+        `,
+    )}
+`;
+
+const ButtonSberPortal = withProps<ButtonProps, HTMLButtonElement>({ size: 's' })(Button);
+const ButtonSberBox = withProps<ButtonProps, HTMLButtonElement>({ size: 'm' })(Button);
+
+const mapDeviceButton: Record<
+    DeviceFamily,
+    React.ForwardRefExoticComponent<ButtonProps & React.RefAttributes<HTMLButtonElement>>
+> = {
+    sberBox: ButtonSberBox,
+    sberPortal: ButtonSberPortal,
+    mobile: ButtonSberPortal,
+};
+
+const StyledButton = styled(mapDeviceButton[deviceFamily])`
+    margin-right: auto;
+`;
+
+const StyledImageContainer = styled.div`
+    margin-left: auto;
+    width: 630px;
+
+    ${mediaQuery(
+        'M',
+        2,
+    )(
+        css`
+            width: 483.46px;
+        `,
+    )}
+`;
+
+const StyledCol = styled(Col)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+export const EmptyCartCommon: React.FC<EmptyCartProps> = ({
+    imageSrc = '',
+    buttonText = defaultButtonText,
+    className,
+    onGoToCatalog,
+}) => {
+    const buttonRef = React.useRef<HTMLButtonElement>(null);
+    useFocusOnMount<HTMLButtonElement>(buttonRef, { prevent: !isSberBox() });
+
+    return (
+        <Row className={className}>
+            <StyledCol sizeXL={5} sizeM={3}>
+                <StyledTitle>В корзине пусто</StyledTitle>
+                {onGoToCatalog && (
+                    <StyledButton ref={buttonRef} view="primary" onClick={onGoToCatalog}>
+                        {buttonText}
+                    </StyledButton>
+                )}
+            </StyledCol>
+            <StyledCol sizeXL={6} offsetXL={1} sizeM={3}>
+                <StyledImageContainer>
+                    <Image base="div" src={imageSrc} ratio="1 / 1" />
+                </StyledImageContainer>
+            </StyledCol>
+        </Row>
+    );
+};
