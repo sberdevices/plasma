7bdb6cc06c0a7c9827d9d4c191b366fa7c21e291
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2021-08-25 11:58:55 +0300

    feat(plasma-temple): Insets with device pixel ratio

--- packages/plasma-temple/src/hooks/useInsets.ts
+++ packages/plasma-temple/src/hooks/useInsets.ts
@@ -12,5 +12,15 @@ export function useInsets(): Insets {
         },
     } = React.useContext(AppStateContext);
 
-    return insets;
+    return React.useMemo(() => {
+        const devicePixelRatio = process.env.NODE_ENV === 'development' ? 1 : window.devicePixelRatio;
+        const { top, bottom, left, right } = insets;
+
+        return {
+            top: top / devicePixelRatio,
+            bottom: bottom / devicePixelRatio,
+            left: left / devicePixelRatio,
+            right: right / devicePixelRatio,
+        };
+    }, [insets]);
 }
