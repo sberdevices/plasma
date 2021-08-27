b735aa0792abba5824b74e2c2a6065d0eec2664b
Fanil Zubairov <fannisco@gmail.com>
2021-08-27 10:22:52 +0200

    fix(plasma-web): Dropdown shadow black

--- packages/plasma-web/src/components/Dropdown/DropdownList.tsx
+++ packages/plasma-web/src/components/Dropdown/DropdownList.tsx
@@ -12,7 +12,7 @@ export const DropdownList = styled.div`
     padding: var(--plasma-dropdown-padding, 0);
 
     background-color: var(--plasma-dropdown-background, ${backgroundPrimary});
-    box-shadow: 0 13px 28px rgba(155, 155, 155, 0.2);
+    box-shadow: 0 0.75rem 1.75rem rgba(0, 0, 0, 0.08);
     border-radius: var(--plasma-dropdown-border-radius, 0);
 
     transition: opacity 0.3s ease-in-out;
