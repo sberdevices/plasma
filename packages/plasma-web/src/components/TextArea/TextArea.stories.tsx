6dff71507e070fa1668b75cb9833a9026cf22985
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2021-08-30 09:33:43 +0300

    docs(plasma-web): Storybook controls for Tabs, TextArea

--- packages/plasma-web/src/components/TextArea/TextArea.stories.tsx
+++ packages/plasma-web/src/components/TextArea/TextArea.stories.tsx
@@ -1,40 +1,90 @@
 import React from 'react';
-import { select, boolean, text } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import { IconPlaceholder, InSpacingDecorator } from '../../helpers';
+import { IconPlaceholder, InSpacingDecorator, disableProps } from '../../helpers';
 
-import { TextArea } from '.';
+import { TextArea, TextAreaProps } from '.';
 
 const onChange = action('onChange');
 const onFocus = action('onFocus');
 const onBlur = action('onBlur');
 
+const statuses = ['', 'success', 'error'];
+const resizes = ['none', 'both', 'horizontal', 'vertical'];
+
+const propsToDisable = [
+    'helperBlock',
+    '$isFocused',
+    'label',
+    'contentRight',
+    'autoComplete',
+    'autoFocus',
+    'cols',
+    'dirName',
+    'form',
+    'minLength',
+    'maxLength',
+    'name',
+    'required',
+    'rows',
+    'value',
+    'wrap',
+    'onChange',
+    'onFocus',
+    'onBlur',
+];
+
 export default {
     title: 'Controls/TextArea',
     component: TextArea,
     decorators: [InSpacingDecorator],
-};
+    argTypes: {
+        status: {
+            control: {
+                type: 'select',
+                options: statuses,
+            },
+        },
+        resize: {
+            control: {
+                type: 'select',
+                options: resizes,
+            },
+        },
+        ...disableProps(propsToDisable),
+    },
+} as Meta;
 
-export const Default = () => {
+export const Default: Story<TextAreaProps & { enableContentRight: boolean }> = ({
+    enableContentRight,
+    status,
+    ...rest
+}) => {
     const [value, setValue] = React.useState('Some text 🌝');
 
     return (
         <TextArea
             value={value}
-            placeholder={text('placeholder', 'Label')}
-            helperText={text('helperText', 'Helper text')}
-            contentRight={boolean('contentRight', true) && <IconPlaceholder />}
-            status={select('status', ['', 'success', 'error'], '') || undefined}
-            resize={select('resize', ['none', 'both', 'horizontal', 'vertical'], 'vertical')}
-            disabled={boolean('disabled', false)}
-            readOnly={boolean('readOnly', false)}
+            contentRight={enableContentRight && <IconPlaceholder />}
+            status={status || undefined}
             onChange={(e) => {
                 setValue(e.target.value);
                 onChange(e);
             }}
             onFocus={onFocus}
             onBlur={onBlur}
+            {...rest}
         />
     );
+};
+
+Default.args = {
+    placeholder: 'Label',
+    helperText: 'Helper text',
+    enableContentRight: true,
+    status: '' as 'success',
+    resize: 'vertical',
+    disabled: false,
+    readOnly: false,
 };
