ed7823fd0f44313f723b79d2806e44d3695e279b
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2021-08-30 13:43:17 +0300

    docs(plasma-web): Storybook controls for Select, Switch, TextField

--- packages/plasma-web/src/components/Switch/Switch.stories.tsx
+++ packages/plasma-web/src/components/Switch/Switch.stories.tsx
@@ -1,38 +1,72 @@
 import React from 'react';
-import { boolean, text } from '@storybook/addon-knobs';
+import { Story, Meta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import styled from 'styled-components';
 
-import { InSpacingDecorator } from '../../helpers';
+import { InSpacingDecorator, disableProps } from '../../helpers';
 
-import { Switch } from '.';
+import { Switch, SwitchProps } from '.';
+
+const propsToDisable = [
+    'id',
+    'onFocus',
+    'onBlur',
+    'onChange',
+    'value',
+    'checked',
+    'description',
+    'focused',
+    'pressed',
+    'outlined',
+    'theme',
+    'as',
+    'forwardedAs',
+];
 
 export default {
     title: 'Controls/Switch',
     component: Switch,
     decorators: [InSpacingDecorator],
-};
+    argTypes: {
+        label: {
+            control: {
+                type: 'text',
+            },
+        },
+        ...disableProps(propsToDisable),
+    },
+} as Meta;
 
 const onChange = action('onChange');
 const onFocus = action('onFocus');
 const onBlur = action('onBlur');
 
-export const Default = () => {
+const StyledWrapper = styled.div`
+    width: 13.75rem;
+`;
+
+export const Default: Story<SwitchProps> = (args) => {
     const value = 0;
     const [checked, setChecked] = React.useState(true);
 
     return (
-        <Switch
-            name={text('name', 'Switch')}
-            value={value}
-            label={text('label', 'Label')}
-            disabled={boolean('disabled', false)}
-            checked={checked}
-            onChange={(event) => {
-                setChecked(event.target.checked);
-                onChange(event);
-            }}
-            onFocus={onFocus}
-            onBlur={onBlur}
-        />
+        <StyledWrapper>
+            <Switch
+                value={value}
+                checked={checked}
+                onChange={(event) => {
+                    setChecked(event.target.checked);
+                    onChange(event);
+                }}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                {...args}
+            />
+        </StyledWrapper>
     );
+};
+
+Default.args = {
+    label: 'Label',
+    disabled: false,
 };
