b12edc395240614331751dfe81ef800d41850b54
Fanil Zubairov <fannisco@gmail.com>
2021-09-02 14:51:46 +0200

    chore(plasma-docs): Fix homepage's main button

--- docs/src/components/Homepage/Header.tsx
+++ docs/src/components/Homepage/Header.tsx
@@ -3,6 +3,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styled from 'styled-components';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import { Container, Headline1, Headline4, Button } from '@sberdevices/plasma-web';
+import { white } from '@sberdevices/plasma-tokens-web';
 
 const StyledHero = styled.div`
     text-align: center;
@@ -11,6 +12,12 @@ const StyledHero = styled.div`
 const StyledContainer = styled(Container)`
     padding-top: 3rem;
     padding-bottom: 3rem;
+`;
+const StyledButton = styled(Button)`
+    &:hover {
+        color: ${white};
+        text-decoration: none;
+    }
 `;
 
 export const HomepageHeader: FC = () => {
@@ -23,9 +30,9 @@ export const HomepageHeader: FC = () => {
                 <Headline1 mb="8x">{siteConfig.title}</Headline1>
                 <Headline4 mb="8x">{siteConfig.tagline}</Headline4>
                 <div>
-                    <Button forwardedAs="a" view="primary" href={mainLink}>
+                    <StyledButton forwardedAs="a" view="primary" href={mainLink}>
                         Начать работу
-                    </Button>
+                    </StyledButton>
                 </div>
             </StyledContainer>
         </StyledHero>
