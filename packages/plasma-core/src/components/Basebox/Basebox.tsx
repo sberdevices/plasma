0daacd78e6300d78216df0f6518df6cf025af8b2
Fanil Zubairov <fannisco@gmail.com>
2021-09-08 14:51:51 +0200

    fix: Refactor `Checkbox` & `Radiobox` in order of better understanding

---
+++ packages/plasma-core/src/components/Basebox/Basebox.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { applyEllipsis, applyNoSelect } from '../../mixins';
+import { secondary } from '../../tokens';
+import type { InputHTMLAttributes } from '../../types';
+import { Body1, Footnote1 } from '../Typography';
+
+export type ControlProps = {
+    /**
+     * Уникальный идентификатор контрола
+     */
+    id?: string;
+    /**
+     * Метка-подпись к элементу
+     */
+    label?: string | number;
+    /**
+     * Описание элемента
+     */
+    description?: string | number | React.ReactNode;
+};
+export interface BaseboxProps extends ControlProps, InputHTMLAttributes<HTMLInputElement> {
+    children?: never;
+}
+
+export const StyledRoot = styled.label`
+    ${applyNoSelect}
+
+    position: relative;
+    align-items: flex-start;
+    display: flex;
+    cursor: pointer;
+`;
+export const StyledInput = styled.input`
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 0;
+    opacity: 0;
+
+    &:focus {
+        outline: 0 none;
+    }
+`;
+export const StyledTrigger = styled.div`
+    box-sizing: border-box;
+    position: relative;
+    transition: all 0.1s ease-in-out;
+`;
+export const StyledContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 0.75rem;
+`;
+export const StyledLabel = styled(Body1)`
+    ${applyEllipsis}
+`;
+export const StyledDescription = styled(Footnote1)`
+    color: ${secondary};
+`;
