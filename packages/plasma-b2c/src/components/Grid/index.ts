97bc04a91dabdc34bfbe71432ac85b11a8c02532
takovoy <bazaster1@gmail.com>
2021-10-13 15:39:15 +0300

    fix: grid import

--- packages/plasma-b2c/src/components/Grid/index.ts
+++ packages/plasma-b2c/src/components/Grid/index.ts
@@ -1,4 +1,4 @@
 export { Row, Col, sizes, offsets } from '@sberdevices/plasma-web';
 export type { ColProps, ColCount, ColSizeProps, ColOffsetProps } from '@sberdevices/plasma-web';
 
-export { Container } from './Container';
+export * from './Container';
