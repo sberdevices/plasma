6df4f160cd4cc054ee6c4311a4dc93dc55b7f3ca
Artem Neretin <hawktrike@gmail.com>
2021-10-14 21:23:58 +0700

    fix(plasma-b2c): Fix re-export from plasma-web for utils

--- packages/plasma-b2c/src/utils/index.ts
+++ packages/plasma-b2c/src/utils/index.ts
@@ -10,15 +10,13 @@ export {
     setRef,
     toCssSize,
     transformStyles,
-    mediaQuery,
-    gridBreakpoints,
-    gridColumns,
-    gridMargins,
-    gridGutters,
-    gridSizes,
 } from '@sberdevices/plasma-core';
 
-export type { TimingFunction, PinProps, MediaQueryFunction, Breakpoint } from '@sberdevices/plasma-core';
+export type { TimingFunction, PinProps, MediaQueryFunction } from '@sberdevices/plasma-core';
+
+export { mediaQuery, gridBreakpoints, gridColumns, gridMargins, gridGutters, gridSizes } from '@sberdevices/plasma-web';
+
+export type { Breakpoint } from '@sberdevices/plasma-web';
 
 export * from './clearSelection';
 export * from './selectText';
