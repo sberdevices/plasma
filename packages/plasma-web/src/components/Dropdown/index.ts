3bc06fec643566bea027057ff5f0dbbd7fe86fe9
Artem Neretin <hawktrike@gmail.com>
2021-10-18 19:26:48 +0700

    chore(plasma-web): Add re-export for `Dropdown` component

--- packages/plasma-web/src/components/Dropdown/index.ts
+++ packages/plasma-web/src/components/Dropdown/index.ts
@@ -1,2 +1,9 @@
-export * from './Dropdown';
-export type { DropdownNode, DropdownItem } from './Dropdown.types';
+export { Dropdown } from './Dropdown';
+export type { DropdownProps } from './Dropdown';
+
+export { DropdownList } from './DropdownList';
+
+export { DropdownItem } from './DropdownItem';
+export type { DropdownItemProps } from './DropdownItem';
+
+export type { DropdownItem as DropdownItemType, DropdownNode as DropdownNodeType } from './Dropdown.types';
