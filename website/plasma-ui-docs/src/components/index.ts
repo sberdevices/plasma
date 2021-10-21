94f4e7f36cb611c78dde41edb09716fffcaa68d7
Fanil Zubairov <fannisco@gmail.com>
2021-10-21 11:01:13 +0200

    chore(plasma-ui-docs): Deleted index page

--- website/plasma-ui-docs/src/components/index.ts
+++ website/plasma-ui-docs/src/components/index.ts
@@ -1,6 +1,3 @@
 export { Description } from './Description';
-export { HomepageFeatures } from './Homepage/Features';
-export { HomepageHeader } from './Homepage/Header';
 export { GlobalStyle } from './GlobalStyle';
 export { PropsTable } from './PropsTable';
-export { RelativeExternalLink } from './RelativeExternalLink';
