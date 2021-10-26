f9c69a163147a5839b3820e68afe0a2afc5a915b
Artem Neretin <hawktrike@gmail.com>
2021-10-26 21:13:47 +0700

    feat(plasma-icons): Update design for `Hearts` icons

--- packages/plasma-icons/src/Icon.assets/Heart.tsx
+++ packages/plasma-icons/src/Icon.assets/Heart.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 import { IconProps } from '../IconRoot';
 
 export const Heart: React.FC<IconProps> = (props) => (
-    <svg width="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg width="100%" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
         <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M12 6.059C6.995.262 3.122 4.266 2.467 5.462c-1.192 2.21-.477 5.557 1.192 7.171l5.686 5.703a3.75 3.75 0 005.312 0l5.686-5.703c1.667-1.614 2.382-4.96 1.19-7.171-.656-1.196-4.767-5.2-9.533.597z"
+            d="M7.36585 4.7998C4.04057 4.7998 2 7.66238 2 10.0103C2 15.0873 7.86207 19.7998 12 22.7998C16.1379 19.7998 22 15.0873 22 10.0103C22 7.66238 19.9594 4.7998 16.6342 4.7998C14.777 4.7998 13.1905 6.2573 12 7.64192C10.8095 6.2573 9.22297 4.7998 7.36585 4.7998Z"
             fill="currentColor"
+            fillOpacity="0.96"
         />
     </svg>
 );
