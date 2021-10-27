9d9816cb2f00ba745ada761ad51c7ed1900daaa1
takovoy <bazaster1@gmail.com>
2021-10-27 19:35:17 +0300

    feat: upload progress label

--- packages/plasma-b2c/src/components/Upload/index.ts
+++ packages/plasma-b2c/src/components/Upload/index.ts
@@ -1,4 +1,6 @@
 export { Upload } from './Upload';
+export { UploadProgress } from './UploadProgress';
 export { defaultValidate } from './utils';
 export type { UploadProps } from './Upload';
+export type { UploadProgressProps } from './UploadProgress';
 export type { ValidationResult } from './types';
