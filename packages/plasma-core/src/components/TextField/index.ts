d5258f1befe76ae711829ab0dc0ee877341e67b9
Fanil Zubairov <fannisco@gmail.com>
2021-11-02 14:07:29 +0200

    feat: Replace `TextField` with `Field`

--- packages/plasma-core/src/components/TextField/index.ts
+++ packages/plasma-core/src/components/TextField/index.ts
@@ -1,9 +1,9 @@
-export type { TextFieldProps, TextAreaProps } from './TextField';
+export type { InputProps as TextFieldProps } from '../Input';
 export {
-    TextFieldRoot,
-    TextFieldInput,
-    TextFieldTextarea,
-    TextFieldPlaceholder,
-    TextFieldContent,
-    TextFieldHelper,
-} from './TextField';
+    FieldRoot as TextFieldRoot,
+    FieldPlaceholder as TextFieldPlaceholder,
+    FieldContent as TextFieldContent,
+    FieldHelper as TextFieldHelper,
+} from '../Field';
+export { Input as TextFieldInput } from '../Input';
+export { TextArea as TextFieldTextarea } from '../TextArea';
