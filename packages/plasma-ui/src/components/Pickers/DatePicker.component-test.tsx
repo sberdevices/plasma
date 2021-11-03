f24f5447fa51593f7448289dfa5472f61505d063
Fanil Zubairov <fannisco@gmail.com>
2021-11-03 16:26:16 +0200

    test(plasma-ui): `DatePicker` and `TimePicker` ct

---
+++ packages/plasma-ui/src/components/Pickers/DatePicker.component-test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount, CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
+
+describe('plasma-ui: DatePicker', () => {
+    const DatePicker = getComponent('DatePicker');
+
+    it('default', () => {
+        mount(
+            <CypressTestDecorator>
+                <DatePicker value={new Date(1980, 8, 1)} min={new Date(1975, 1, 1)} max={new Date(1985, 10, 30)} />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+});
---
+++ packages/plasma-ui/src/components/Pickers/TimePicker.component-test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount, CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
+
+describe('plasma-ui: TimePicker', () => {
+    const TimePicker = getComponent('TimePicker');
+
+    it('default', () => {
+        mount(
+            <CypressTestDecorator>
+                <TimePicker
+                    value={new Date(1980, 8, 1, 0, 28, 59)}
+                    min={new Date(1975, 1, 1, 0, 15, 29)}
+                    max={new Date(1985, 10, 30, 12, 30, 30)}
+                />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+});
