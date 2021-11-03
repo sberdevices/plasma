2cb9b11bf1686783db8245d70643fd91cc7c2311
Aleksandr Bulakh <aabulakh@sberbank.ru>
2021-11-03 11:29:53 +0300

    feat(plasma-b2c): make single slider rail clickable

--- packages/plasma-b2c/src/components/Slider/SliderBase.tsx
+++ packages/plasma-b2c/src/components/Slider/SliderBase.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled, { css } from 'styled-components';
 import { surfaceLiquid03, buttonAccent } from '@sberdevices/plasma-core';
 
@@ -15,6 +15,7 @@ interface SliderProps {
     railFillXPosition?: number;
     disabled?: boolean;
     setStepSize(stepSize: number): void;
+    onChange?: (value: number, data: { lastX: number }) => void;
 }
 
 const Slider = styled.div<{ disabled?: boolean }>`
@@ -28,6 +29,12 @@ const Slider = styled.div<{ disabled?: boolean }>`
         `}
 `;
 
+const RailWrap = styled.div`
+    height: 100%;
+    margin-left: ${indentaion}rem;
+    margin-right: ${indentaion}rem;
+`;
+
 const Rail = styled.div`
     position: relative;
     height: ${railHeight}rem;
@@ -35,8 +42,6 @@ const Rail = styled.div`
     background-color: ${surfaceLiquid03};
     overflow: hidden;
     top: 50%;
-    margin-left: ${indentaion}rem;
-    margin-right: ${indentaion}rem;
     transform: translateY(-50%);
 `;
 
@@ -57,14 +62,27 @@ export const SliderBase: React.FC<SliderProps> = ({
     children,
     railFillXPosition = 0,
     disabled,
+    onChange,
 }) => {
     const ref = React.useRef<HTMLDivElement | null>(null);
+
+    const onHandleChange: React.MouseEventHandler<HTMLDivElement> = useCallback(
+        (e) => {
+            if (!onChange) return;
+            const { x, width } = e.currentTarget.getBoundingClientRect();
+            const lastX = e.clientX - x;
+            const position = min + (lastX / width) * (max - min);
+            const result = Math.max(min, Math.min(max, position));
+
+            onChange(result, { lastX });
+        },
+        [onChange, min, max],
+    );
 
     React.useLayoutEffect(() => {
         const resizeHandler = () => {
             if (ref.current) {
-                const rootElementFontSize = 16;
-                const railSize = ref.current.offsetWidth - indentaion * rootElementFontSize * 2;
+                const railSize = ref.current.offsetWidth;
                 const totalSteps = max - min;
                 setStepSize(railSize / totalSteps);
             }
@@ -75,10 +93,12 @@ export const SliderBase: React.FC<SliderProps> = ({
     }, [min, max, setStepSize]);
 
     return (
-        <Slider disabled={disabled} ref={ref}>
-            <Rail>
-                <Fill style={{ left: `${railFillXPosition}px`, width: `${railFillWidth}px` }} />
-            </Rail>
+        <Slider disabled={disabled}>
+            <RailWrap ref={ref} onMouseDown={onHandleChange}>
+                <Rail>
+                    <Fill style={{ left: `${railFillXPosition}px`, width: `${railFillWidth}px` }} />
+                </Rail>
+            </RailWrap>
             {children}
         </Slider>
     );
