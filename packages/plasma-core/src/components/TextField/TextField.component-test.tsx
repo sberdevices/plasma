8b7441735ee32ff7511b4846552916a1d8f2d17a
Fanil Zubairov <fannisco@gmail.com>
2021-11-03 12:10:41 +0200

    test: Improve tests for `TextField` and `TextArea`

--- packages/plasma-core/src/components/TextField/TextField.component-test.tsx
+++ packages/plasma-core/src/components/TextField/TextField.component-test.tsx
@@ -1,13 +1,95 @@
 import React from 'react';
-import { mount, CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
+import { mount, CypressTestDecorator, getComponent, SpaceMe } from '@sberdevices/plasma-cy-utils';
+import { IconSleep, IconEye } from '@sberdevices/plasma-icons';
 
 describe('plasma-core: TextField', () => {
     const TextField = getComponent('TextField');
 
-    it('simple', () => {
+    it('default', () => {
         mount(
             <CypressTestDecorator>
-                <TextField value="Welcome" />
+                <TextField value="Value" placeholder="Placeholder" helperText="Helper text" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it(':empty', () => {
+        mount(
+            <CypressTestDecorator>
+                <TextField placeholder="Placeholder" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it(':focused', () => {
+        mount(
+            <CypressTestDecorator>
+                <TextField value="Value" placeholder="Placeholder" helperText="Helper text" />
+            </CypressTestDecorator>,
+        );
+
+        cy.get('input:first').focus();
+        cy.matchImageSnapshot();
+    });
+
+    it(':disabled', () => {
+        mount(
+            <CypressTestDecorator>
+                <TextField value="Value" placeholder="Placeholder" helperText="Helper text" disabled />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it(':success, :warning, :error', () => {
+        mount(
+            <CypressTestDecorator>
+                <TextField value="Value" placeholder="Placeholder" helperText="Helper text" status="success" />
+                <SpaceMe />
+                <TextField value="Value" placeholder="Placeholder" helperText="Helper text" status="warning" />
+                <SpaceMe />
+                <TextField value="Value" placeholder="Placeholder" helperText="Helper text" status="error" />
+                <SpaceMe />
+                <TextField placeholder="Placeholder" status="success" />
+                <SpaceMe />
+                <TextField placeholder="Placeholder" status="warning" />
+                <SpaceMe />
+                <TextField placeholder="Placeholder" status="error" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('content', () => {
+        mount(
+            <CypressTestDecorator>
+                <TextField
+                    value="Value"
+                    placeholder="Placeholder"
+                    helperText="Helper text"
+                    contentLeft={<IconSleep color="inherit" size="s" />}
+                />
+                <SpaceMe />
+                <TextField
+                    value="Value"
+                    placeholder="Placeholder"
+                    helperText="Helper text"
+                    contentLeft={<IconSleep color="inherit" size="s" />}
+                    contentRight={<IconEye color="inherit" size="s" />}
+                />
+                <SpaceMe />
+                <TextField
+                    value="Value"
+                    placeholder="Placeholder"
+                    helperText="Helper text"
+                    contentRight={<IconEye color="inherit" size="s" />}
+                />
             </CypressTestDecorator>,
         );
 
