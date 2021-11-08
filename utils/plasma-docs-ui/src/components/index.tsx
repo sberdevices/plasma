f747e59509ff870aef06e3eddc7374c6cb088e76
Fanil Zubairov <fannisco@gmail.com>
2021-11-08 15:48:38 +0200

    feat(plasma-docs-ui): Added GlobalStyle component

--- utils/plasma-docs-ui/src/components/index.tsx
+++ utils/plasma-docs-ui/src/components/index.tsx
@@ -1,6 +1,7 @@
 export { Description } from './Description';
 export type { DescriptionProps } from './Description';
 export { Filler } from './Filler';
+export { GlobalStyle } from './GlobalStyle';
 export { PropsTable } from './PropsTable';
 export type { PropsTableProps } from './PropsTable';
 export { PlaygroundPreview } from './Playground';
