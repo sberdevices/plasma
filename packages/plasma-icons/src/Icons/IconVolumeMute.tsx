64111b9de3f8b2eb515281f5e39784f0856ec974
Artem Neretin <hawktrike@gmail.com>
2021-11-08 15:41:56 +0700

    fix(plasma-icons): Remove IconVolumeMute icon

--- packages/plasma-icons/src/Icons/IconVolumeMute.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { VolumeOff } from '../Icon.assets/VolumeOff';
-import { IconRoot, IconProps } from '../IconRoot';
-
-export const IconVolumeOff: React.FC<IconProps> = ({ size = 's', color, className }) => {
-    return <IconRoot className={className} size={size} color={color} icon={VolumeOff} />;
-};
