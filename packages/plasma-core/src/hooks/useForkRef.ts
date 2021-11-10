ebb611366b538e6ee803d929175bb9779d88da22
Fanil Zubairov <fannisco@gmail.com>
2021-11-10 15:44:09 +0200

    docs(plasma-ui): Added documentation for `useForkRef`

--- packages/plasma-core/src/hooks/useForkRef.ts
+++ packages/plasma-core/src/hooks/useForkRef.ts
@@ -6,6 +6,12 @@ export interface UseForkRefHook {
     <T>(refOne: React.Ref<T>, refTwo: React.Ref<T>): React.Ref<T>;
 }
 
+/**
+ * Позволяет переиспользовать объект `ref` внутри forwardRef.
+ * @param {React.Ref<T>} refOne
+ * @param {React.Ref<T>} refTwo
+ * @return {Function React.Ref}
+ */
 export const useForkRef: UseForkRefHook = (refOne, refTwo) => {
     return React.useMemo(() => {
         if (refOne == null && refTwo === null) {
