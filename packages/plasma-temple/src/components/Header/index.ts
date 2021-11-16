31f71fb8ad1c103bd3102ff4d655b7174ad9b31e
Ivan Kudryavtsev <vanyakudr007@mail.ru>
2021-11-17 00:28:26 +0300

    fix(plasma-temple): useHeaderProps export

---
+++ packages/plasma-temple/src/components/Header/index.ts
@@ -0,0 +1,2 @@
+export { Header } from './Header';
+export { useHeaderProps } from './useHeaderProps';
--- packages/plasma-temple/src/components/index.ts
+++ packages/plasma-temple/src/components/index.ts
@@ -3,7 +3,7 @@ export * from './Page';
 export * from './MediaPlayer';
 export * from './Form';
 export * from './Product';
-export * from './Header/Header';
+export * from './Header';
 export * from './GalleryCard';
 export * from './Gallery';
 export * from './HeroSlider';
