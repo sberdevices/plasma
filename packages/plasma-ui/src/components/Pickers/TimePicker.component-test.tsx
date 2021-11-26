4bf13a08251f5e5dab5318828032da543da04435
Artem Neretin <hawktrike@gmail.com>
2021-11-26 19:57:29 +0700

    test(plasma-ui): Add tests for `DatePicker` and `TimePicker` components and update snapshots

--- packages/plasma-ui/src/components/Pickers/TimePicker.component-test.tsx
+++ packages/plasma-ui/src/components/Pickers/TimePicker.component-test.tsx
@@ -17,4 +17,19 @@ describe('plasma-ui: TimePicker', () => {
 
         cy.matchImageSnapshot();
     });
+
+    it('without infiniteScroll', () => {
+        mount(
+            <CypressTestDecorator>
+                <TimePicker
+                    infiniteScroll={false}
+                    value={new Date(1980, 8, 1, 0, 28, 59)}
+                    min={new Date(1975, 1, 1, 0, 15, 29)}
+                    max={new Date(1985, 10, 30, 12, 30, 30)}
+                />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
 });
