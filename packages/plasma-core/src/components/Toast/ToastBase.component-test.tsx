82284c094f49e7bfee7e8e458ed1e429e02cdd9e
Vasiliy Loginevskiy <yeti.or@gmail.com>
2021-12-12 20:35:11 +0300

    test(plasma-ui): add Toast component tests

--- packages/plasma-core/src/components/Toast/ToastBase.component-test.tsx
+++ packages/plasma-core/src/components/Toast/ToastBase.component-test.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import { mount } from '@cypress/react';
-import { CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
+import { mount, CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
 import { IconClose } from '@sberdevices/plasma-icons';
 import { critical } from '@sberdevices/plasma-tokens-b2c';
 
 describe('plasma-core: Toast', () => {
     const Toast = getComponent('Toast');
+    const useToast = getComponent('useToast');
+    const ToastProvider = getComponent('ToastProvider');
 
     it('default', () => {
         mount(
             <CypressTestDecorator>
                 <Toast text="Short Text Message Without Action" />
             </CypressTestDecorator>,
-            { ReactDom },
         );
 
         cy.matchImageSnapshot();
     });
 
-    it('with icon', () => {
+    it('__content-left', () => {
         mount(
             <CypressTestDecorator>
                 <Toast
@@ -27,9 +26,93 @@ describe('plasma-core: Toast', () => {
                     contentLeft={<IconClose size="xs" color={critical} />}
                 />
             </CypressTestDecorator>,
-            { ReactDom },
         );
 
         cy.matchImageSnapshot();
+    });
+
+    it('_role', () => {
+        mount(
+            <CypressTestDecorator>
+                <Toast role="alert" text="Short Text Message Without Action" />
+                <Toast role="log" text="Short Text Message Without Action" />
+                <Toast role="status" text="Short Text Message Without Action" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    describe('useToast', () => {
+        const text = 'Short Text Message Without Action';
+        const Button = getComponent('Button');
+
+        const Interactive = ({ text: _text, position, timeout }) => {
+            const { showToast } = useToast();
+
+            return <Button id="show" text="show" onClick={() => showToast(_text, position, timeout)} />;
+        };
+
+        it('showToast', () => {
+            mount(
+                <CypressTestDecorator>
+                    <ToastProvider>
+                        <Interactive text={text} />
+                    </ToastProvider>
+                </CypressTestDecorator>,
+            );
+
+            cy.get('#show').click();
+
+            // eslint-disable-next-line cypress/no-unnecessary-waiting
+            cy.wait(300);
+            cy.matchImageSnapshot();
+        });
+
+        it('_position', () => {
+            mount(
+                <CypressTestDecorator>
+                    <ToastProvider>
+                        <Interactive text={text} position="top" />
+                    </ToastProvider>
+                </CypressTestDecorator>,
+            );
+
+            cy.get('#show').click();
+
+            // eslint-disable-next-line cypress/no-unnecessary-waiting
+            cy.wait(300);
+            cy.matchImageSnapshot();
+        });
+
+        it('dumm', () => {
+            mount(
+                <CypressTestDecorator>
+                    <ToastProvider>
+                        <Interactive />
+                    </ToastProvider>
+                </CypressTestDecorator>,
+            );
+
+            cy.get('#show').click();
+
+            cy.matchImageSnapshot();
+        });
+
+        it('timeout', () => {
+            mount(
+                <CypressTestDecorator>
+                    <ToastProvider>
+                        <Interactive text={text} timeout={0} />
+                    </ToastProvider>
+                </CypressTestDecorator>,
+            );
+
+            cy.get('#show').click();
+
+            // eslint-disable-next-line cypress/no-unnecessary-waiting
+            cy.wait(600);
+            cy.matchImageSnapshot();
+        });
     });
 });
