4fd4b9895268877a809c333518f4f60786c6c398
Vasiliy Loginevskiy <yeti.or@gmail.com>
2021-12-12 20:35:11 +0300

    test(plasma-ui): add Switch component tests

--- packages/plasma-core/src/components/Switch/Switch.component-test.tsx
+++ packages/plasma-core/src/components/Switch/Switch.component-test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
+import { mount, CypressTestDecorator, getComponent, PadMe } from '@sberdevices/plasma-cy-utils';
 
 describe('plasma-core: Switch', () => {
     const Switch = getComponent('Switch');
@@ -12,5 +12,68 @@ describe('plasma-core: Switch', () => {
         );
 
         cy.matchImageSnapshot();
+    });
+
+    it('_checked', () => {
+        mount(
+            <CypressTestDecorator>
+                <Switch checked label="Переключатель" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('_focused', () => {
+        mount(
+            <CypressTestDecorator>
+                <Switch focused label="Переключатель" />
+                <PadMe />
+                <Switch focused checked label="Переключатель" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('_disabled', () => {
+        mount(
+            <CypressTestDecorator>
+                <Switch disabled focused label="Переключатель" />
+                <PadMe />
+                <PadMe />
+                <Switch disabled focused checked label="Переключатель" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('_pressed', () => {
+        mount(
+            <CypressTestDecorator>
+                <Switch label="Переключатель" />
+                <PadMe />
+                <Switch pressed label="Переключатель" />
+                <PadMe />
+                <Switch checked label="Переключатель" />
+                <PadMe />
+                <Switch pressed checked label="Переключатель" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('onClick', () => {
+        mount(
+            <CypressTestDecorator>
+                <Switch id="switch" label="Переключатель" />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+        cy.get('#switch').click();
+        cy.matchImageSnapshot('clicked');
     });
 });
