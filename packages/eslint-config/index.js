44f1b30661f2413de4e612d8f3547049d79bbd6c
Oleg Ozeritskiy <ovozeritskiy@sberbank.ru>
2021-12-13 15:09:35 +0300

    feat(eslint-config): add eslint-config-base with TS/JS rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,62 +1,12 @@
 module.exports = {
     extends: [
         'airbnb',
-        'airbnb/hooks',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:cypress/recommended',
         'prettier/react',
+        '@sberdevices/eslint-config-base',
     ],
     parser: '@typescript-eslint/parser',
-    plugins: ['react', 'import', 'prettier', 'cypress'],
+    plugins: ['react'],
     rules: {
-        '@typescript-eslint/no-empty-function': 'off',
-        'no-restricted-syntax': 'off', // В for...of циклах ничего плохого нет
-        'spaced-comment': ['error', 'always', { markers: ['/'] }], /// разрешаем ts-require directive
-        'comma-dangle': ['error', 'always-multiline'],
-        'arrow-parens': ['error', 'always'],
-
-        'space-before-function-paren': [
-            'error',
-            {
-                anonymous: 'never',
-                named: 'never',
-                asyncArrow: 'always',
-            },
-        ],
-        indent: 'off',
-        'max-len': [
-            'error',
-            120,
-            2,
-            {
-                ignoreUrls: true,
-                ignoreComments: false,
-                ignoreRegExpLiterals: true,
-                ignoreStrings: true,
-                ignoreTemplateLiterals: true,
-            },
-        ],
-        'padding-line-between-statements': [
-            'error',
-            { blankLine: 'always', prev: '*', next: 'return' },
-            { blankLine: 'always', prev: '*', next: 'if' },
-        ],
-        'implicit-arrow-linebreak': 'off',
-        'no-plusplus': 'off',
-        'max-classes-per-file': 'off',
-        'operator-linebreak': 'off',
-        'object-curly-newline': 'off',
-        'class-methods-use-this': 'off',
-        'no-confusing-arrow': 'off',
-        'function-paren-newline': 'off',
-        'no-param-reassign': 'off',
-        'no-shadow': 'warn',
-        'consistent-return': 'off',
-        'prettier/prettier': 'error',
-
-        '@typescript-eslint/explicit-function-return-type': 'off',
-
         'react/prop-types': 'off',
         'react/static-property-placement': 'off',
         'react/state-in-constructor': 'off',
@@ -72,20 +22,6 @@ module.exports = {
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-noninteractive-tabindex': 'off',
-
-        'import/prefer-default-export': 'off', // @grape: https://humanwhocodes.com/blog/2019/01/stop-using-default-exports-javascript-module/
-        'import/order': [
-            'error',
-            {
-                groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
-                'newlines-between': 'always',
-            },
-        ],
-        'import/no-unresolved': 'off',
-        'import/extensions': 'off',
-        'import/no-extraneous-dependencies': ['off'],
-        'arrow-body-style': 'off',
-        'no-unused-expressions': 'off',
     },
     overrides: [
         {
@@ -97,16 +33,7 @@ module.exports = {
                 window: true,
                 document: true,
             },
-        },
-        {
-            files: ['*.test.tsx?', '*.test.js'],
-            plugins: ['jest'],
-            env: {
-                browser: true,
-                mocha: true,
-                'jest/globals': true,
-            },
-        },
+        }
     ],
     settings: {
         react: {
