dc82767ef54600b0ecd27c5709c86761543967c8
Fanil Zubairov <fannisco@gmail.com>
2021-12-13 17:45:19 +0200

    feat(plasma-b2c): Added `TabsController` component

--- packages/plasma-b2c/src/components/Tabs/index.ts
+++ packages/plasma-b2c/src/components/Tabs/index.ts
@@ -1,4 +1,8 @@
+export { TabsController } from './TabsController';
+export type { TabsControllerProps } from './TabsController';
+
 export { Tabs } from './Tabs';
 export type { TabsProps } from './Tabs';
+
 export { TabItem } from './TabItem';
 export type { TabItemProps } from './TabItem';
