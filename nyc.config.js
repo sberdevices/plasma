d56ef2fd43c5187a8106d412e83151bd7ffa0b90
Vasiliy Loginevskiy <yeti.or@gmail.com>
2021-12-15 20:39:08 +0300

    ci: merge coverage

--- nyc.config.js
+++ nyc.config.js
@@ -5,14 +5,14 @@ let nycConfig = {};
 if (coverageDir) {
     nycConfig = {
         'report-dir': coverageDir,
-        'temp-dir': `.nyc_output/${coverageDir}`,
+        // 'temp-dir': `.nyc_output/${coverageDir}`,
         include: [`**/packages/${process.env.PACKAGE_NAME}/**`],
         excludeAfterRemap: true,
     };
 
-    if (process.env.PACKAGE_NAME === 'plasma-ui') {
-        nycConfig.include.push('**/packages/plasma-core/**');
-    }
+    // if (process.env.PACKAGE_NAME === 'plasma-ui') {
+    nycConfig.include.push('**/packages/plasma-core/**');
+    // }
 }
 
 module.exports = nycConfig;
