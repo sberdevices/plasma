e7c7eba7efe800949d43ed20fc2781b3588a78fa
Artem Neretin <hawktrike@gmail.com>
2021-12-22 21:07:12 +0700

    test(plasma-b2c): Add tests and snapshots for `Upload` component

--- packages/plasma-b2c/src/components/Upload/Upload.component-test.tsx
+++ packages/plasma-b2c/src/components/Upload/Upload.component-test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { mount, CypressTestDecorator, getComponent, PadMe } from '@sberdevices/plasma-cy-utils';
 import { IconPicture } from '@sberdevices/plasma-icons';
 
+import { defaultValidate } from './utils';
+import { ValidationResult } from './types';
+
 describe('plasma-b2c: Upload', () => {
     const Upload = getComponent('Upload');
 
@@ -14,6 +17,48 @@ describe('plasma-b2c: Upload', () => {
                 <Upload />
             </CypressTestDecorator>,
         );
+
+        cy.matchImageSnapshot();
+    });
+
+    function Demo() {
+        const onChange = React.useCallback(() => {}, []);
+
+        const onValidation = React.useCallback(() => {}, []);
+
+        const validate = React.useCallback((files: FileList | null): ValidationResult => {
+            return {
+                message: `Добавлен файл формата ${files[0].type}`,
+                status: 'success',
+                data: files[0],
+            };
+        }, []);
+
+        return <Upload content="Drag and Drop" onChange={onChange} validate={validate} onValidation={onValidation} />;
+    }
+
+    it('upload dnd from fixture', () => {
+        const fileUrl = 'images/320_320_0.jpg';
+        const type = 'image/jpg';
+
+        mount(
+            <CypressTestDecorator>
+                <Demo />
+            </CypressTestDecorator>,
+        );
+
+        cy.fixture(fileUrl, 'base64')
+            .then(Cypress.Blob.base64StringToBlob)
+            .then((blob) => {
+                const nameSegments = fileUrl.split('/');
+                const name = nameSegments[nameSegments.length - 1];
+                const testFile = new File([blob], name, { type });
+                const dataTransfer = new DataTransfer();
+                dataTransfer.items.add(testFile);
+                const event = { dataTransfer };
+
+                return cy.get('.file-drop').trigger('drop', event);
+            });
 
         cy.matchImageSnapshot();
     });
@@ -95,5 +140,41 @@ describe('plasma-b2c: Upload', () => {
         );
 
         cy.matchImageSnapshot();
+    });
+});
+
+describe('plasma-b2c: Upload utils', () => {
+    const Upload = getComponent('Upload');
+
+    const files = ([
+        {
+            name: 'video.mkv',
+        },
+    ] as unknown) as FileList;
+
+    it('defaultValidate', () => {
+        mount(
+            <CypressTestDecorator>
+                <Upload />
+            </CypressTestDecorator>,
+        );
+
+        expect(defaultValidate(null)).to.deep.eq({
+            message: 'Загрузите файл',
+            status: 'error',
+            data: null,
+        });
+
+        expect(defaultValidate(files)).to.deep.eq({ data: { name: 'video.mkv' } });
+
+        expect(defaultValidate(files, '.mkv')).to.deep.eq({ data: { name: 'video.mkv' } });
+
+        expect(defaultValidate(files, '.avi, .mkv')).to.deep.eq({ data: { name: 'video.mkv' } });
+
+        expect(defaultValidate(files, '.avi')).to.deep.eq({
+            data: null,
+            message: 'Неверный формат файла. Используйте avi-формат',
+            status: 'error',
+        });
     });
 });
