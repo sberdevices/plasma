e9a58787698ba97566e1c8c8a69cdf2d7950247d
Artem Neretin <hawktrike@gmail.com>
2021-12-27 20:12:25 +0700

    test(plasma-b2c): Add test and snapshot for `ElasticGrid` component

--- packages/plasma-b2c/src/components/ElasticGrid/ElasticGrid.component-test.tsx
+++ packages/plasma-b2c/src/components/ElasticGrid/ElasticGrid.component-test.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { mount, CypressTestDecorator } from '@sberdevices/plasma-cy-utils';
-
-import { ElasticGrid } from '.';
-
-const StyledElasticGrid = styled(ElasticGrid)`
-    width: 600px;
-`;
+import { mount, CypressTestDecorator, PadMe, getComponent } from '@sberdevices/plasma-cy-utils';
 
 const Item = styled.div`
     width: 100%;
@@ -15,7 +9,17 @@ const Item = styled.div`
     border-radius: 10px;
 `;
 
+const items = [
+    { id: 0, label: '0' },
+    { id: 1, label: '1' },
+    { id: 2, label: '2' },
+    { id: 3, label: '3' },
+    { id: 4, label: '4' },
+];
+
 describe('ElasticGrid', () => {
+    const ElasticGrid = getComponent('ElasticGrid');
+
     before(() => {
         cy.on('uncaught:exception', (err) => {
             if (err.toString().match(/ResizeObserver loop limit exceeded/)) {
@@ -27,15 +31,20 @@ describe('ElasticGrid', () => {
     it('_view', () => {
         mount(
             <CypressTestDecorator>
-                <StyledElasticGrid minColWidth={125} gapX={8} gapY={8}>
-                    <Item />
-                    <Item />
-                    <Item />
-                    <Item />
-                    <Item />
-                </StyledElasticGrid>
+                <ElasticGrid minColWidth={125} gapX={8} gapY={8}>
+                    {items.map((item) => (
+                        <Item key={item.id}>{item.label}</Item>
+                    ))}
+                </ElasticGrid>
+                <PadMe />
+                <ElasticGrid minColWidth={125}>
+                    {items.map((item) => (
+                        <Item key={item.id}>{item.label}</Item>
+                    ))}
+                </ElasticGrid>
             </CypressTestDecorator>,
         );
+
         cy.matchImageSnapshot();
     });
 });
