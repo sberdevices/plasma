156f65887db5eb382baed82268f7551a8a4aa722
VladislavPetyukevich <pet.vladislav@gmail.com>
2021-12-28 10:02:35 +0300

    fix(plasma-temple): improve CartPage mobile markup

--- packages/plasma-temple/src/pages/CartPage/CartPage.stories.tsx
+++ packages/plasma-temple/src/pages/CartPage/CartPage.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { action } from '@storybook/addon-actions';
 import { tertiary } from '@sberdevices/plasma-tokens';
-import { Footnote1 } from '@sberdevices/plasma-ui';
+import { Footnote1, mediaQuery } from '@sberdevices/plasma-ui';
 
 import { CartPage } from './CartPage';
 import { CartProvider } from './components/CartProvider/CartProvider';
@@ -75,6 +75,10 @@ const StyledText = styled(Footnote1)`
     margin-top: 1rem;
     color: ${tertiary};
     text-align: center;
+
+    ${mediaQuery('S')`
+        text-align: left;
+    `}
 `;
 
 export const Default = (): React.ReactElement => {
