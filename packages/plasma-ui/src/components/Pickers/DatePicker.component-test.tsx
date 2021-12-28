093b03ee0e687d25259aa5df2b10762121df4cd7
Artem Neretin <hawktrike@gmail.com>
2021-12-28 21:22:11 +0700

    test(plasma-ui): Add tests and snapshots for `Pickers` component

--- packages/plasma-ui/src/components/Pickers/DatePicker.component-test.tsx
+++ packages/plasma-ui/src/components/Pickers/DatePicker.component-test.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 import React from 'react';
 import { mount, CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
+
+const noop = () => {};
 
 describe('plasma-ui: DatePicker', () => {
     const DatePicker = getComponent('DatePicker');
@@ -25,6 +28,31 @@ describe('plasma-ui: DatePicker', () => {
                 />
             </CypressTestDecorator>,
         );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('change values', () => {
+        mount(
+            <CypressTestDecorator>
+                <DatePicker
+                    scrollSnapType="none"
+                    onChange={noop}
+                    value={new Date(1985, 8, 1)}
+                    min={new Date(1975, 1, 1)}
+                    max={new Date(1995, 10, 30)}
+                />
+            </CypressTestDecorator>,
+        );
+
+        // отключение анимаций на всех div'ах внутри окружения, TODO: перенести в plasma-cy-utils?
+        cy.get('div').invoke('attr', 'style', 'transition: unset; animation: none; scroll-snap-type: none;');
+
+        cy.contains('5').click({ force: true });
+        cy.contains('мая').click({ force: true });
+        cy.contains('1979').click({ force: true });
+
+        cy.wait(1000);
 
         cy.matchImageSnapshot();
     });
