11886ac3f886a149233deff161eda3fe35e4368f
Artem Neretin <hawktrike@gmail.com>
2021-12-28 19:16:02 +0700

    test(plasma-ui): Add tests and snapshots for `Slider` component

--- packages/plasma-ui/src/components/Slider/Slider.component-test.tsx
+++ packages/plasma-ui/src/components/Slider/Slider.component-test.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { mount, CypressTestDecorator, getComponent } from '@sberdevices/plasma-cy-utils';
+import { mount, CypressTestDecorator, getComponent, PadMe } from '@sberdevices/plasma-cy-utils';
+
+const noop = () => {};
+
+const dragAndDrop = (chainableSelector: Cypress.Chainable, coord: { clientX: number; clientY: number }) => {
+    chainableSelector.trigger('mousedown').trigger('mousemove', coord).trigger('mouseup');
+};
 
 describe('plasma-ui: Slider', () => {
     const Slider = getComponent('Slider');
@@ -14,12 +20,64 @@ describe('plasma-ui: Slider', () => {
         cy.matchImageSnapshot();
     });
 
+    it('_disabled', () => {
+        mount(
+            <CypressTestDecorator>
+                <Slider disabled value={42} min={0} max={100} />
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
+
     it('double', () => {
         mount(
             <CypressTestDecorator>
                 <Slider value={[13, 42]} min={0} max={100} />
             </CypressTestDecorator>,
         );
+
+        cy.matchImageSnapshot();
+    });
+
+    it('drag and drop with single', () => {
+        mount(
+            <CypressTestDecorator>
+                <Slider onChange={noop} onChangeCommitted={noop} value={50} min={0} max={100} />
+            </CypressTestDecorator>,
+        );
+
+        dragAndDrop(cy.root(), { clientX: 350, clientY: 15 });
+
+        cy.matchImageSnapshot();
+    });
+
+    it('drag and drop without onChange', () => {
+        mount(
+            <CypressTestDecorator>
+                <Slider onChangeCommitted={noop} value={[0, 50]} min={0} max={100} />
+                <PadMe />
+                <Slider onChangeCommitted={noop} value={50} min={0} max={100} />
+            </CypressTestDecorator>,
+        );
+
+        dragAndDrop(cy.root().get('div > div + div').first(), { clientX: 50, clientY: 15 });
+
+        dragAndDrop(cy.root().get('div > div + div').last(), { clientX: 350, clientY: 55 });
+
+        cy.matchImageSnapshot();
+    });
+
+    it('drag and drop with double', () => {
+        mount(
+            <CypressTestDecorator>
+                <Slider onChange={noop} onChangeCommitted={noop} value={[25, 75]} min={0} max={100} />
+            </CypressTestDecorator>,
+        );
+
+        dragAndDrop(cy.root().get('div > div + div').first(), { clientX: 50, clientY: 15 });
+
+        dragAndDrop(cy.root().get('div > div + div').last(), { clientX: 450, clientY: 15 });
 
         cy.matchImageSnapshot();
     });
