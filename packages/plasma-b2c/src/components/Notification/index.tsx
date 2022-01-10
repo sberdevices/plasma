fd4d97ad76e6ac5eaefdce74a3827ab259e18eb1
Artem Neretin <hawktrike@gmail.com>
2022-01-10 19:29:09 +0700

    fix(plasma-b2c,plasma-web): Add re-export for `Notification` component

--- packages/plasma-b2c/src/components/Notification/index.tsx
+++ packages/plasma-b2c/src/components/Notification/index.tsx
@@ -1,2 +1,3 @@
-export { NotificationsProvider } from '@sberdevices/plasma-web';
+export { NotificationsProvider, Notification } from '@sberdevices/plasma-web';
 export { addNotification, closeNotification } from '@sberdevices/plasma-web';
+export type { NotificationProps } from '@sberdevices/plasma-web';
--- packages/plasma-web/src/components/Notification/index.tsx
+++ packages/plasma-web/src/components/Notification/index.tsx
@@ -1,2 +1,4 @@
 export { NotificationsProvider } from './NotificationsProvider';
+export { Notification } from './Notification';
 export { addNotification, closeNotification } from './NotificationsStore';
+export type { NotificationProps } from './Notification';
