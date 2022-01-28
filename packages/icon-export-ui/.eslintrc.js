d3fc3c6953443c4c5c17221d2db117ccfcedc3db
Artem Neretin <hawktrike@gmail.com>
2022-01-28 19:54:03 +0700

    chore(icon-export-ui): Update infrastructure configs

--- packages/icon-export-ui/.eslintrc.js
+++ packages/icon-export-ui/.eslintrc.js
@@ -6,5 +6,7 @@ module.exports = {
         '@typescript-eslint/no-use-before-define': ['error'],
         'react/require-default-props': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
     },
 };
