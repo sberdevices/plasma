8106d61ff903997f79277427f477387d8afcbdad
Fanil Zubairov <fannisco@gmail.com>
2022-01-28 15:40:17 +0200

    feat(plasma-ui-docs, plasma-web-docs): Added Storybook links on doc pages

--- website/plasma-web-docs/src/components/index.ts
+++ website/plasma-web-docs/src/components/index.ts
@@ -2,3 +2,4 @@ export { CodeSandbox } from './CodeSandbox';
 export { Description } from './Description';
 export { GlobalStyle } from './GlobalStyle';
 export { PropsTable } from './PropsTable';
+export { StorybookLink } from './Storybook';
