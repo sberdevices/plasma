cf489aef15e27037d62e4e694a26e6f0e605905d
Лев Останин <lsostanin@sberbank.ru>
2022-02-09 13:25:39 +0300

    feat(plasma-ui): Made overlay optional for Sheet

--- packages/plasma-ui/src/components/Sheet/Sheet.stories.tsx
+++ packages/plasma-ui/src/components/Sheet/Sheet.stories.tsx
@@ -8,17 +8,42 @@ import { Sheet } from './Sheet';
 
 export default {
     title: 'Content/Sheet',
-    component: Sheet,
     decorators: [InSpacing],
 };
 
-export const Default = () => {
+export const Default = ({ withOverlay }) => {
     const [isOpen, setIsOpen] = React.useState(false);
 
     return (
         <>
             <Button onClick={() => setIsOpen(true)}>Открыть</Button>
-            <Sheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
+            <Sheet isOpen={isOpen} withOverlay={withOverlay} onClose={() => setIsOpen(false)}>
+                <Body1>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae tempore vitae porro laboriosam
+                    consectetur fugiat assumenda, earum nesciunt. Distinctio minima nesciunt dicta rem quae vel illum ea
+                    fugit molestiae dolorem? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos nostrum
+                    placeat, neque repudiandae consectetur voluptates soluta et sint eum obcaecati nesciunt ullam,
+                    dolorem labore quaerat vero maxime ab ipsa nihil.
+                </Body1>
+            </Sheet>
+        </>
+    );
+};
+
+Default.argTypes = {
+    withOverlay: {
+        control: { type: 'boolean' },
+        defaultValue: true,
+    },
+};
+
+export const WithoutOverlay = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    return (
+        <>
+            <Button onClick={() => setIsOpen(true)}>Открыть</Button>
+            <Sheet isOpen={isOpen} withOverlay={false} onClose={() => setIsOpen(false)}>
                 <Body1>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae tempore vitae porro laboriosam
                     consectetur fugiat assumenda, earum nesciunt. Distinctio minima nesciunt dicta rem quae vel illum ea
