d9cb4af319d311093f9903a27b3e205ca27520d0
Fanil Zubairov <fannisco@gmail.com>
2022-02-18 23:30:47 +0200

    chore(plasma-web): Support `plasma-typo` in Storybook

--- packages/plasma-web/.storybook/preview.js
+++ packages/plasma-web/.storybook/preview.js
@@ -7,6 +7,7 @@ import { light as b2bLight, dark as b2bDark } from '@sberdevices/plasma-tokens-b
 import { light as b2cLight, dark as b2cDark } from '@sberdevices/plasma-tokens-b2c/themes';
 import { light as legacyLight, dark as legacyDark } from '@sberdevices/plasma-tokens-web/themes';
 import { b2b as b2bTypo } from '@sberdevices/plasma-tokens-b2b/typo';
+import { standard as standardTypo, compatible as compatibleTypo } from '@sberdevices/plasma-typo';
 
 import { ToastProvider } from '../src/components/Toast';
 
@@ -36,31 +37,52 @@ const DocumentStyle = createGlobalStyle`
 `;
 /* stylelint-enable */
 
-const TypoStyle = createGlobalStyle(b2bTypo);
+const OldTypo = createGlobalStyle(b2bTypo);
+const TypoStyle = createGlobalStyle(standardTypo);
+const CompatibleTypoStyle = createGlobalStyle(compatibleTypo);
+
+const B2B_LIGHT_THEME = 'b2b:light';
+const B2B_DARK_THEME = 'b2b:dark';
+const B2C_LIGHT_THEME = 'b2c:light';
+const B2C_DARK_THEME = 'b2c:dark';
+const LEGACY_LIGHT_THEME = 'light (legacy)';
+const LEGACY_DARK_THEME = 'dark (legacy)';
 
 const themes = {
-    'b2b:light': createGlobalStyle(b2bLight),
-    'b2b:dark': createGlobalStyle(b2bDark),
-    'b2c:light': createGlobalStyle(b2cLight),
-    'b2c:dark': createGlobalStyle(b2cDark),
-    'light (legacy)': createGlobalStyle(legacyLight),
-    'dark (legacy)': createGlobalStyle(legacyDark),
+    [B2B_LIGHT_THEME]: createGlobalStyle(b2bLight),
+    [B2B_DARK_THEME]: createGlobalStyle(b2bDark),
+    [B2C_LIGHT_THEME]: createGlobalStyle(b2cLight),
+    [B2C_DARK_THEME]: createGlobalStyle(b2cDark),
+    [LEGACY_LIGHT_THEME]: createGlobalStyle(legacyLight),
+    [LEGACY_DARK_THEME]: createGlobalStyle(legacyDark),
 };
 
 const withTheme = (Story, context) => {
+    let typoSystem = 'responsive';
     let theme = context.globals.theme;
 
     if (theme === 'light') {
-        theme = 'light (legacy)';
+        theme = LEGACY_LIGHT_THEME;
     } else if (theme === 'dark') {
-        theme = 'dark (legacy)';
+        theme = LEGACY_DARK_THEME;
+    }
+
+    if (theme === LEGACY_LIGHT_THEME || theme === LEGACY_DARK_THEME) {
+        typoSystem = 'leagacy';
     }
 
     const Theme = themes[theme];
 
     return (
         <>
-            <TypoStyle />
+            {typoSystem === 'responsive' ? (
+                <>
+                    <TypoStyle />
+                    <CompatibleTypoStyle />
+                </>
+            ) : (
+                <OldTypo />
+            )}
             <Theme />
             <DocumentStyle />
             <Story {...context} />
@@ -118,7 +140,14 @@ export const globalTypes = {
         description: 'Global theme for components',
         defaultValue: 'b2b:light',
         toolbar: {
-            items: ['b2b:light', 'b2b:dark', 'b2c:light', 'b2c:dark', 'light (legacy)', 'dark (legacy)'],
+            items: [
+                B2B_LIGHT_THEME,
+                B2B_DARK_THEME,
+                B2C_LIGHT_THEME,
+                B2C_DARK_THEME,
+                LEGACY_LIGHT_THEME,
+                LEGACY_DARK_THEME,
+            ],
             showName: true,
         },
     },
