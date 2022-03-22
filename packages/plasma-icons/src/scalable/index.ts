46655cacbe89c95df80385dd3d74aacad0b82676
Artem Neretin <hawktrike@gmail.com>
2022-03-22 18:14:50 +0700

    feat(plasma-icons): Add new root files for scalable icons

---
+++ packages/plasma-icons/src/scalable/index.ts
@@ -0,0 +1,11 @@
+/**
+ * ToDo: ! Использовать данные иконки после https://github.com/sberdevices/plasma/issues/1058 в v2.0
+ */
+export { IconVideo } from './Icons/IconVideo';
+export { IconAttentionCircle } from './Icons/IconAttentionCircle';
+export { IconAntennaCircle } from './Icons/IconAntennaCircle';
+
+export { IconRoot } from './IconRoot';
+export type { IconProps, IconSize } from './IconRoot';
+
+export { Icon } from './Icon';
