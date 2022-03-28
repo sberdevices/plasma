8cae7e2b442dfc9b27f4407e303929d3bcabf721
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2022-03-28 15:43:11 +0300

    feat(plasma-temple): Assistant impovements

--- packages/plasma-temple/src/assistant.ts
+++ packages/plasma-temple/src/assistant.ts
@@ -1,62 +1,45 @@
-import { AssistantSmartAppData, createAssistant, createAssistantDev } from '@sberdevices/assistant-client';
+import { AssistantSmartAppData, createAssistant, createSmartappDebugger } from '@sberdevices/assistant-client';
 
 import { AssistantInstance, PickOptional } from './types';
 import { logger } from './utils/logger';
 
-export type AssistantProps = Parameters<typeof createAssistantDev>[0];
+export type AssistantProps = Parameters<typeof createSmartappDebugger>[0];
 
-export type InitializeParams = PickOptional<
-    AssistantProps,
-    'token' | 'getRecoveryState' | 'url' | 'userChannel' | 'surface'
-> &
-    Pick<AssistantProps, 'initPhrase' | 'nativePanel' | 'getState'>;
+export type InitializeParams = PickOptional<AssistantProps, 'token'> & Omit<AssistantProps, 'token'>;
 
 let assistant: AssistantInstance;
 
+// @deprecated use getAssistantRef
 export const getAssistant = (): AssistantInstance => assistant;
+
+export interface AssistantRef {
+    assistant: AssistantInstance | null;
+}
+
+export const getAssistantRef = (): AssistantRef => ({
+    assistant: assistant ?? null,
+});
 
 export const initializeAssistant = <T extends AssistantSmartAppData>({
     getState,
     getRecoveryState,
-    initPhrase,
-    nativePanel,
-    token,
-    url = 'wss://vpstest2.online.sberbank.ru:443/vpsdemo2/',
-    userChannel = 'B2C',
-    surface = 'SBERBOX',
+    token = '',
+    ...restProps
 }: InitializeParams): AssistantInstance => {
     if (process.env.NODE_ENV === 'development' && !window.Cypress) {
-        const environmentProps = token
-            ? {
-                  userChannel: 'B2C',
-                  surface: 'SBERBOX',
-                  url: 'wss://nlp2vps.online.sberbank.ru/vps/',
-                  settings: {
-                      authConnector: 'developer_portal_jwt',
-                  },
-              }
-            : {
-                  url,
-                  userChannel,
-                  surface,
-              };
-
-        assistant = createAssistantDev<T>({
+        assistant = createSmartappDebugger<T>({
             getState: () => {
                 const state = getState();
                 logger('Assistant state', state);
                 return state;
             },
-            getRecoveryState,
-            initPhrase,
-            nativePanel,
             token,
-            ...environmentProps,
+            ...restProps,
         });
 
         assistant.on('data', (action) => logger('Assistant Action', action));
     } else {
-        assistant = createAssistant<T>({ getState });
+        assistant = createAssistant<T>({ getState, getRecoveryState });
     }
 
     return assistant;
