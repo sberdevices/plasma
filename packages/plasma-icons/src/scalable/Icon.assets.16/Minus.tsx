6a2c3338806ab5036ff73fb4ab500720bd6296a6
neretinaa <hawktrike@gmail.com>
2022-03-28 16:39:55 +0700

    feat(plasma-icon): Add icons for 16 dp sizes

---
+++ packages/plasma-icons/src/scalable/Icon.assets.16/Minus.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { IconProps } from '../IconRoot';
+
+export const Minus: React.FC<IconProps> = (props) => (
+    <svg width="100%" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path fillRule="evenodd" clipRule="evenodd" d="M2 8C2 7.44772 2.44772 7 3 7H13C13.5523 7 14 7.44772 14 8C14 8.55228 13.5523 9 13 9H3C2.44772 9 2 8.55228 2 8Z" fill="currentColor" />
+    </svg>
+);
