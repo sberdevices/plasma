1e81431ecc362cf17a044fa519d65f52316cbdec
Konstantin Polyanskiy <kv.polyanskiy@gmail.com>
2022-03-29 16:38:36 +0300

    feat(plasma-temple): update build

--- packages/plasma-temple/babel.config.js
+++ packages/plasma-temple/babel.config.js
@@ -1,7 +1,59 @@
-module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-react',
-        '@babel/preset-typescript',
+const namespace = process.env.SC_NAMESPACE;
+
+const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    'babel-plugin-annotate-pure-calls',
+    [
+        'babel-plugin-styled-components',
+        {
+            displayName: false,
+            namespace,
+        },
     ],
+];
+
+const ignore = [
+    '**/*.d.ts',
+    '**/testHelpers',
+    '**/*.stories.tsx',
+    '**/*.examples.tsx',
+    '**/*.test.ts',
+    '**/*.test.tsx',
+    '**/*.spec.ts',
+    '**/*.spec.tsx',
+    '**/*.component-test.ts',
+    '**/*.component-test.tsx',
+];
+
+process.env.COVERAGE && plugins.push(['babel-plugin-istanbul']);
+
+module.exports = {
+    env: {
+        cjs: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            plugins,
+            ignore,
+        },
+        esm: {
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        modules: false,
+                    },
+                ],
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+            ],
+            plugins,
+            ignore,
+        },
+        test: {
+            presets: [
+                ['@babel/preset-env', { targets: { node: 'current' } }],
+                '@babel/preset-react',
+                '@babel/preset-typescript',
+            ],
+        },
+    },
 };
