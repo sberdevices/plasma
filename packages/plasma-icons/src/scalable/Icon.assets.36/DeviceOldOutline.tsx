b24869802c39f19bc2a4e8ab99f901e9c3013200
neretinaa <hawktrike@gmail.com>
2022-03-30 15:25:48 +0700

    feat(plasma-icons): Add icons for `36 dp` from G to O

---
+++ packages/plasma-icons/src/scalable/Icon.assets.36/DeviceOldOutline.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { IconProps } from '../IconRoot';
+
+export const DeviceOldOutline: React.FC<IconProps> = (props) => (
+    <svg width="100%" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5 9H31L31 27H5.00001L5 9ZM3 9C3 7.89543 3.89543 7 5 7H31C32.1046 7 33 7.89543 33 9L33 27C33 28.1046 32.1046 29 31 29H5.00001C3.89545 29 3.00002 28.1046 3.00002 27L3 9ZM15.12 21.42C14.72 21.7 14.17 21.67 13.82 21.32C13.39 20.89 13.42 20.17 13.91 19.82C15.06 18.99 16.47 18.5 18 18.5C19.53 18.5 20.94 18.99 22.09 19.82C22.57 20.18 22.61 20.89 22.18 21.32C21.83 21.67 21.28 21.69 20.88 21.41C20.07 20.84 19.07 20.5 18 20.5C16.93 20.5 15.93 20.84 15.12 21.42ZM11.05 16.97C12.94 15.43 15.36 14.5 18 14.5C20.64 14.5 23.06 15.43 24.94 16.98C25.4 17.36 25.44 18.05 25.02 18.47C24.66 18.83 24.09 18.86 23.7 18.54C22.16 17.27 20.17 16.5 18 16.5C15.83 16.5 13.84 17.27 12.29 18.53C11.9 18.85 11.33 18.82 10.97 18.46C10.55 18.04 10.59 17.35 11.05 16.97Z"
+            fill="currentColor"
+        />
+        <defs>
+            <linearGradient
+                id="paint0Linear2233267"
+                x1="2.45494"
+                y1="74.1726"
+                x2="33.2807"
+                y2="21.5238"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#20DA03" />
+                <stop offset="1" stopColor="#11DDFF" />
+            </linearGradient>
+        </defs>
+    </svg>
+);
