2ed78cc96262921799276b24e3a507bd06d0c0f9
neretinaa <hawktrike@gmail.com>
2022-03-30 14:26:36 +0700

    feat(plasma-icons): Add icons for `36 dp` from A to E

---
+++ packages/plasma-icons/src/scalable/Icon.assets.36/Mobile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { IconProps } from '../IconRoot';
+
+export const Mobile: React.FC<IconProps> = (props) => (
+    <svg width="100%" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M24 31H12C10.3437 31 9 29.6563 9 28V8C9 6.34372 10.3437 5 12 5H24C25.6563 5 27 6.34372 27 8V28C27 29.6563 25.6563 31 24 31ZM24 29C24.5517 29 25 28.5517 25 28V8C25 7.44828 24.5517 7 24 7H21.5033C21.5033 7.82843 20.8317 8.5 20.0033 8.5H16.0002C15.1717 8.5 14.5002 7.82843 14.5002 7H12C11.4483 7 11 7.44828 11 8V28C11 28.5517 11.4483 29 12 29M17 27H19C19.5523 27 20 26.5523 20 26C20 25.4477 19.5523 25 19 25H17C16.4477 25 16 25.4477 16 26C16 26.5523 16.4477 27 17 27Z"
+            fill="currentColor"
+        />
+    </svg>
+);
